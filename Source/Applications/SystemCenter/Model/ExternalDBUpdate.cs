e76560959d0c30b980ed2d72782cbcbefdad4ac4
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-04-09 16:46:03 -0400

    Added Model for AdditionalFields that are changed by the user for updating external DB

---
+++ Source/Applications/SystemCenter/Model/ExternalDBUpdate.cs
@@ -0,0 +1,49 @@
+﻿//******************************************************************************************************
+//  ExternalDBUpdate.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  04/08/2020 - Christoph Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using GSF.Data;
+using GSF.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using SystemCenter.Controllers;
+
+namespace SystemCenter.Model
+{
+    public class ExternalDBUpdate
+    {
+        public int? ID { get; set; }
+        public int OpenXDAParentTableID { get; set; }
+        public int AdditionalFieldID { get; set; }
+        public string Value { get; set; }
+        public string FieldName { get; set; }
+        public string PreviousValue { get; set; }
+        public bool Error { get; set; }
+    }
+
+    
+
+
+    
+
+}
