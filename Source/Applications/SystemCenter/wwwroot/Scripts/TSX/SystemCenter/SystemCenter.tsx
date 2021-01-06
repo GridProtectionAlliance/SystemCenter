b104d5393247837147d7d3d451e4fe4625122963
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-01-06 11:33:11 -0500

    Fixed TS to compile

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/SystemCenter.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/SystemCenter.tsx
@@ -78,7 +78,7 @@ const SystemCenter: React.FunctionComponent = (props: {}) => {
     React.useEffect(() => {
         let handle = getRoles();
         handle.done(rs => setRoles(rs));
-        window.addEventListener('resize', (evt) => forceUpdate(1));
+        window.addEventListener('resize', (evt) => forceUpdate());
 
         return function cleanup() {
             if (handle.abort != null)
