7654c0d737a243d98a094ee64ca9c0db77df1092
seanaroche <seanaroche7@gmail.com>
2021-07-15 14:57:54 -0400

    Created a tooltip error for when the Tap field is non numeric when creating a Transformer

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetAttribute/Asset.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetAttribute/Asset.tsx
@@ -290,6 +290,8 @@ export namespace AssetAttributes {
                 errors.push('Secondary Voltage needs to be numeric.')
             if ((asset as OpenXDA.Transformer).TertiaryVoltageKV != null && !AssetAttributes.isRealNumber((asset as OpenXDA.Transformer).TertiaryVoltageKV))
                 errors.push('Tertiary Voltage needs to be numeric.')
+            if ((asset as OpenXDA.Transformer).Tap != null && !AssetAttributes.isRealNumber((asset as OpenXDA.Transformer).Tap))
+                errors.push('Tap needs to be numeric.')
             if ((asset as OpenXDA.Transformer).PrimaryWinding != null && !AssetAttributes.isRealNumber((asset as OpenXDA.Transformer).PrimaryWinding))
                 errors.push('Primary Winding needs to be numeric.')
             if ((asset as OpenXDA.Transformer).SecondaryWinding != null && !AssetAttributes.isRealNumber((asset as OpenXDA.Transformer).SecondaryWinding))
