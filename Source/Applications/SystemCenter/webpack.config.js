c179c3175a5e27f5e1b2f06e3b48f1e067ced390
Billy Ernest <bernest@gridprotectionalliance.org>
2021-09-20 10:47:47 -0400

    Updated prebuild events and webpack configs to build differently on debug and release.

--- Source/Applications/SystemCenter/webpack.config.js
+++ Source/Applications/SystemCenter/webpack.config.js
@@ -7,7 +7,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = env => {
     if (env.NODE_ENV == undefined) env.NODE_ENV = 'development';
 
-
+    if(env.NODE_ENV == 'development')
     return {
         mode: env.NODE_ENV,
         context: path.resolve(__dirname),
@@ -66,5 +66,57 @@ module.exports = env => {
             new NodePolyfillPlugin(),
             new ForkTsCheckerWebpackPlugin()
         ]
-    }
+        }
+    else
+        return {
+            mode: env.NODE_ENV,
+            context: path.resolve(__dirname),
+            cache: true,
+            entry: {
+                SystemCenter: "./wwwroot/Scripts/TSX/SystemCenter/SystemCenter.tsx",
+            },
+
+            output: {
+                path: path.resolve(__dirname, 'wwwroot', 'Scripts'),
+                publicPath: 'Scripts/',
+                filename: "[name].js"
+                //chunkFilename: '[name].bundle.js'
+            },
+            // Enable sourcemaps for debugging webpack's output.
+            devtool: "inline-source-map",
+            resolve: {
+                // Add '.ts' and '.tsx' as resolvable extensions.
+                extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".css"],
+                alias: {
+                }
+            },
+            module: {
+                rules: [
+                    // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+                    {
+                        test: /\.tsx?$/,
+                        include: path.resolve(__dirname, 'wwwroot', "Scripts"),
+                        loader: "ts-loader", options: { transpileOnly: true }
+                    },
+                    {
+                        test: /\.css$/,
+                        include: path.resolve(__dirname, 'wwwroot', "Content"),
+                        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+                    },
+                ]
+            },
+            externals: {
+                ace: 'ace',
+                d3: 'd3',
+            },
+            optimization: {
+                minimizer: [
+                    new TerserPlugin({ extractComments: false })
+                ],
+            },
+            plugins: [
+                new NodePolyfillPlugin(),
+            ]
+        }
+
 };
