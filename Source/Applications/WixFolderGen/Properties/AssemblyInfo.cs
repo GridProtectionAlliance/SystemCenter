7b00b7b8c2071c76be6bb721539b9568fd5bcab7
gsfbuildbot <support@gridprotectionalliance.org>
2021-10-19 00:07:41 -0400

    SystemCenter: Version change for build v3.0.73.69-master.

--- Source/Applications/WixFolderGen/Properties/AssemblyInfo.cs
+++ Source/Applications/WixFolderGen/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.72.69")]
-[assembly: AssemblyVersion("3.0.72.69")]
-[assembly: AssemblyFileVersion("3.0.72.69")]
+// [assembly: AssemblyVersion("3.0.73.69")]
+[assembly: AssemblyVersion("3.0.73.69")]
+[assembly: AssemblyFileVersion("3.0.73.69")]
