2c3771ee103f8470347cde75200b4f4865292fbb
Samuel Robinson <srobinson@gridprotectionalliance.org>
2021-11-02 15:59:20 -0400

    updated to a function component / removed useless functions/ added QueryForm.tsx contents to fix unneccesary seperation

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/QueryWindow.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/QueryWindow.tsx
@@ -26,43 +26,49 @@ import * as React from 'react';
 import * as _ from 'lodash';
 import ExternalDBForm from './ExternalDBForm';
 declare var homePath: string;
-import { OpenXDA } from '@gpa-gemstone/application-typings'
-import QueryForm from './QueryForm';
+import { SystemCenter } from '@gpa-gemstone/application-typings'
+import { TextArea } from '@gpa-gemstone/react-forms';
 
-export default class QueryWindow extends React.Component<{ ExternalDB: OpenXDA.Types.ExternalDataBase, stateSetter: (externalDB: OpenXDA.Types.ExternalDataBase) => void }, { ExternalDB: OpenXDA.Types.ExternalDataBase}, {}> {
-    jqueryHandle: JQuery.jqXHR;
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            ExternalDB: this.props.ExternalDB
-        }
-    }
+interface IProps {
+    ExternalDB: SystemCenter.Types.ExternalDataBaseTable;
+    stateSetter: (externalDB: SystemCenter.Types.ExternalDataBaseTable) => void
+    setErrors?: (e: string[]) => void
+}
 
+export default function QueryWindow(props: IProps) {
 
-    componentDidMount() {
-    }
+    const [extDBTable, setExtDBTable] = React.useState<SystemCenter.Types.ExternalDataBaseTable>(props.ExternalDB)
 
-    componentWillReceiveProps(nextProps): void {
-        this.setState({ ExternalDB: nextProps.ExternalDB})
-    }
+    React.useEffect(() => {
+        let e = [];
+        if (props.ExternalDB.Query != null && props.ExternalDB.Query.length > 200)
+            e.push('Query must be less than 200 characters.');
+        if (props.setErrors != undefined)
+            props.setErrors(e);
+    }, [props.ExternalDB]);
 
-    updateExternalDatabase(): JQuery.jqXHR {
-        var externalDB = _.clone(this.state.ExternalDB);
+    function updateExternalDatabase(): JQuery.jqXHR {
+        var externalDB = _.clone(extDBTable);
 
        return $.ajax({
             type: "PATCH",
            url: `${homePath}api/OpenXDA/ExternalDBTables/Update`,
             contentType: "application/json; charset=utf-8",
-           data: JSON.stringify(this.state.ExternalDB),
+           data: JSON.stringify(extDBTable),
             dataType: 'json',
             cache: true,
             async: true
        }).done((LocationID: number) => {
-           this.props.stateSetter(externalDB);
+           props.stateSetter(externalDB);
        });
     }
 
-    render() {
+    function Valid(field: keyof (SystemCenter.Types.ExternalDataBaseTable)): boolean {
+        if (field == 'Query')
+            return props.ExternalDB.Query != null && props.ExternalDB.Query.length > 0 && props.ExternalDB.Query.length <= 200;
+        return false;
+    }
+
         return (
             <div className="card" style={{ marginBottom: 10 }}>
                 <div className="card-header">
@@ -73,19 +79,18 @@ export default class QueryWindow extends React.Component<{ ExternalDB: OpenXDA.T
                     </div>
                 </div>
                 <div className="card-body">
-                    <QueryForm ExternalDB={this.state.ExternalDB} Setter={(record) => this.setState({ ExternalDB: record })} />
+                    <form>
+                        <TextArea<SystemCenter.Types.ExternalDataBaseTable> Rows={10} Record={props.ExternalDB} Field={'Query'} Feedback={props.ExternalDB.Query == null ? 'Query must be greater than 0 characters.' : 'Query must be less than 200 characters'} Valid={Valid} Setter={props.stateSetter} />
+                    </form>
                 </div>
                 <div className="card-footer">
                     <div className="btn-group mr-2">
-                        <button className="btn btn-primary" onClick={() => this.updateExternalDatabase()} hidden={this.state.ExternalDB.ID == 0} disabled={this.state.ExternalDB == this.props.ExternalDB}>Update</button>
+                        <button className="btn btn-primary" onClick={() => updateExternalDatabase()} hidden={extDBTable.ID == 0} disabled={extDBTable == props.ExternalDB}>Save Changes</button>
                     </div>
                     <div className="btn-group mr-2">
-                        <button className="btn btn-default" onClick={() => this.setState({ ExternalDB: this.props.ExternalDB })} disabled={this.state.ExternalDB == this.props.ExternalDB}>Reset</button>
+                        <button className="btn btn-default" onClick={() => setExtDBTable(props.ExternalDB)} disabled={extDBTable == props.ExternalDB}>Clear Changes</button>
                     </div>
                 </div>
-
-
             </div>
         );
     }
-}
