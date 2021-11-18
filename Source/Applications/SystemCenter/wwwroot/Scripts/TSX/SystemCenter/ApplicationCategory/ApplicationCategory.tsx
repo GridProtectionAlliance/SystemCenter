aa77c22c5c7540d7909b296821a03343fb0f63b4
Samuel Robinson <srobinson@gridprotectionalliance.org>
2021-11-18 15:37:34 -0500

    Added TabSelector and info for each tab as well as a function to get the tab. Added Applications Tab.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ApplicationCategory/ApplicationCategory.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ApplicationCategory/ApplicationCategory.tsx
@@ -1,5 +1,5 @@
 ﻿//******************************************************************************************************
-//  ByApplicationCategory.tsx - Gbtc
+//  ApplicationCategory.tsx - Gbtc
 //
 //  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
 //
@@ -23,18 +23,22 @@
 import { ApplicationCategory } from '../ApplicationCategory/ByApplicationCategory';
 import * as React from 'react';
 import * as _ from 'lodash';
-import { LoadingScreen, Warning } from '@gpa-gemstone/react-interactive';
+import { LoadingScreen, TabSelector, Warning } from '@gpa-gemstone/react-interactive';
 import { useDispatch, useSelector } from 'react-redux';
 import { ApplicationCategorySlice } from '../Store/Store';
 import { Application } from '@gpa-gemstone/application-typings';
 import { Input } from '@gpa-gemstone/react-forms';
 import ApplicationCategoryInfo from './ApplicationCategoryInfo';
+import Applications from './Applications';
 
 declare var homePath: string;
+type tab = 'appCatInfo' | 'applications'
 
-function ApplicationCategory(props: { ID: number }) {
+interface IProps { ID: number, Tab: tab }
 
-    const [tab, setTab] = React.useState('Name')
+function ApplicationCategory(props: IProps) {
+
+    const [tab, setTab] = React.useState(getTab());
     const [showDelete, setShowDelete] = React.useState<boolean>(false);
     const [loadDelete, setLoadDelete] = React.useState<boolean>(false);
     const acStatus = useSelector(ApplicationCategorySlice.Status) as Application.Types.Status;
@@ -47,8 +51,20 @@ function ApplicationCategory(props: { ID: number }) {
             dispatch(ApplicationCategorySlice.Fetch());
     }, [acStatus]);
 
-    if (applicationCategory == null) return null;
+    const Tabs: { Id: tab, Label: string }[] = [
+        { Id: "appCatInfo", Label: "Application Category Info" },
+        { Id: "applications", Label: "Applications" },
+    ];
 
+    function getTab(): tab {
+        if (props.Tab != undefined) return props.Tab;
+        else if (sessionStorage.hasOwnProperty('ApplicationCategory.Tab'))
+            return JSON.parse(sessionStorage.getItem('ApplicationCategory.Tab'));
+        else
+            return 'appCatInfo';
+    }
+
+    if (applicationCategory == null) return null;
     return (
         <div style={{ width: '100%', height: window.innerHeight - 63, maxHeight: window.innerHeight - 63, overflow: 'hidden', padding: 15 }}>
             <div className="row">
@@ -59,17 +75,14 @@ function ApplicationCategory(props: { ID: number }) {
                     <button className="btn btn-danger pull-right" hidden={applicationCategory == null} onClick={() => { setShowDelete(true);} }>Delete Application Category</button>
                 </div>
             </div>
-
-            <hr />
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <a className={"nav-link" + (tab == "Name" ? " active" : "")} onClick={() => setTab('Name')} data-toggle="tab">Application Category Information</a>
-                </li>
-            </ul>
+            <TabSelector CurrentTab={tab} SetTab={(t: tab) => setTab(t)} Tabs={Tabs} />
 
             <div className="tab-content" style={{ maxHeight: window.innerHeight - 235, overflow: 'hidden' }}>
-                <div className={"tab-pane " + (tab == "Name" ? " active" : "fade")} id="Name">
+                <div className={"tab-pane " + (tab == "appCatInfo" ? " active" : "fade")} id="appCatInfo">
                     <ApplicationCategoryInfo ApplicationCat={applicationCategory} stateSetter={(record) => dispatch(ApplicationCategorySlice.DBAction({verb: 'PATCH', record: record}))} />
+                </div>
+                <div className={"tab-pane " + (tab == "applications" ? " active" : "fade")} id="applications">
+                    <Applications ID={props.ID} Tab={tab}/>
                 </div>
             </div>
 
