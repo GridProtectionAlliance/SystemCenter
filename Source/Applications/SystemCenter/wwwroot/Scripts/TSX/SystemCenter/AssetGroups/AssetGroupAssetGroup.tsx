31a19d7d1238fa624c16c576d55b79e76d0251e5
Samuel Robinson <srobinson@gridprotectionalliance.org>
2021-12-07 15:33:55 -0500

    Fixed addtogroup pop up to include the changes made in AddToGroup.tsx

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetGroups/AssetGroupAssetGroup.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetGroups/AssetGroupAssetGroup.tsx
@@ -27,7 +27,9 @@ import * as _ from 'lodash';
 import { OpenXDA } from '@gpa-gemstone/application-typings';
 import { useHistory } from 'react-router-dom';
 import Table from '@gpa-gemstone/react-table';
-import AddToGroupPopup from './AddToGroup';
+import AddToGroup from './AddToGroup';
+import { AssetGroupSlice } from '../Store/Store';
+import { Modal } from '@gpa-gemstone/react-interactive';
 
 declare var homePath: string;
 
@@ -131,8 +133,25 @@ function AssetGroupAssetGroupWindow(props: { AssetGroupID: number}) {
             </div>
             <div className="card-footer">
                     <button className="btn btn-primary" onClick={() => setShowAdd(true)}>Add Asset Group</button>
-                </div>
-                <AddToGroupPopup type='Group' onComplete={AddGroups} Show={showAdd} Close={() => setShowAdd(false)} />
+            </div>
+                <Modal Show={showAdd} Size={'xlg'} ShowX={true} ShowCancel={false} ConfirmBtnClass={'btn-danger'} ConfirmText={'Close'} Title={''} CallBack={() => setShowAdd(false)}>
+                    <AddToGroup<OpenXDA.Types.AssetGroup> Type='Asset Group' Slice={AssetGroupSlice} Data={groupList} SetData={(d) => setGroupList(d)} InitialSortKey={'Name' as keyof OpenXDA.Types.AssetGroup} StandardSearch={{ label: 'Name', key: 'Name', type: 'string', isPivotField: false }}
+                    DefaultFilterList={[
+                        { label: 'Name', key: 'Name', type: 'string', isPivotField: false },
+                        { label: 'Number of Meter', key: 'Meters', type: 'integer', isPivotField: false },
+                        { label: 'Number of Transmission Assets', key: 'Assets', type: 'integer', isPivotField: false },
+                        { label: 'Number of Users', key: 'Users', type: 'integer', isPivotField: false },
+                        { label: 'Show in PQ Dashboard', key: 'DisplayDashboard', type: 'boolean', isPivotField: false },
+                    ]}
+                    TableColumns={[
+                        { key: 'Name', field: 'Name', label: 'Name', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                        { key: 'Assets', field: 'Assets', label: 'Assets', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                        { key: 'Meters', field: 'Meters', label: 'Meters', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                        { key: 'Users', field: 'Users', label: 'Users', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                        { key: 'AssetGroups', field: 'AssetGroups', label: 'SubGroups', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                        { key: 'Scroll', label: '', headerStyle: { width: 17, padding: 0 }, rowStyle: { width: 0, padding: 0 } },
+                        ]} />
+                </Modal>
             </div>
             </>
     );
