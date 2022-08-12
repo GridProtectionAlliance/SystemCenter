e3c2cfc3a0a6af358df534dcdc9aa0df5eaf6d94
Christoph Lackner <clackner@gridprotectionalliance.org>
2022-08-12 11:06:53 -0400

    Added Cell Carrier UI

---
+++ Source/Applications/SystemCenterNotification/Scripts/TSX/CellCarrier/ByCellCarrier.tsx
@@ -0,0 +1,155 @@
+﻿//******************************************************************************************************
+//  ByCellCarrier.tsx - Gbtc
+//
+//  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/05/2022 - C. Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+import { useDispatch, useSelector } from 'react-redux';
+import * as React from 'react';
+import { LoadingScreen, Modal, Search, SearchBar } from '@gpa-gemstone/react-interactive'
+import { CrossMark, HeavyCheckMark } from '@gpa-gemstone/gpa-symbols';
+import { Application } from '@gpa-gemstone/application-typings';
+import Table from '@gpa-gemstone/react-table';
+import moment from 'moment';
+import { ICellCarrier } from '../global';
+import { CellCarrierSlice } from '../Store';
+import { Input } from '@gpa-gemstone/react-forms';
+import { castArray } from 'lodash';
+
+
+declare var homePath;
+declare var version;
+
+interface IProps {}
+
+const ByCellCarrier = (props: IProps) => {
+    const dispatch = useDispatch();
+
+    const searchFields: Search.IField<ICellCarrier>[] = [
+        { key: "Name", label: "Name", type: "string", isPivotField: false },
+        { key: "Transform", label: "Transform", type: "string", isPivotField: false },
+    ]
+
+    const searchStatus: Application.Types.Status = useSelector(CellCarrierSlice.SearchStatus);
+    const status: Application.Types.Status = useSelector(CellCarrierSlice.Status);
+    const data: ICellCarrier[] = useSelector(CellCarrierSlice.SearchResults);
+    const allData: ICellCarrier[] = useSelector(CellCarrierSlice.Data);
+    const sortField = useSelector(CellCarrierSlice.SortField);
+    const asc = useSelector(CellCarrierSlice.Ascending);
+    const filters = useSelector(CellCarrierSlice.SearchFilters);
+    const [showModal, setShowModal] = React.useState<'New'|'Edit'|'Hide'>('Hide');
+    const [carrier, setCarrier] = React.useState<ICellCarrier>({ Name: '', Transform: '', ID: 0 })
+
+
+    React.useEffect(() => {
+        if (searchStatus == 'unintiated' || searchStatus == 'changed')
+            dispatch(CellCarrierSlice.DBSearch({ filter: filters, sortField, ascending: asc }));
+    }, [searchStatus])
+
+    React.useEffect(() => {
+        if (status == 'unintiated' || status == 'changed')
+            dispatch(CellCarrierSlice.Fetch());
+    }, [status])
+
+    return (
+        <>
+            <LoadingScreen Show={status === 'loading'} />
+            <div style={{ width: '100%', height: '100%' }}>
+                <SearchBar<ICellCarrier> CollumnList={searchFields}
+                    SetFilter={(flds) => dispatch(CellCarrierSlice.DBSearch({ filter: flds }))}
+                    Direction={'left'} defaultCollumn={{ key: 'Name', label: 'Name', type: 'string', isPivotField: false }} Width={'50%'} Label={'Search'}
+                    ShowLoading={searchStatus === 'loading'} ResultNote={searchStatus === 'error' ? 'Could not complete Search' : 'Found ' + data.length + ' Cell Carrier(s)'}
+                    GetEnum={() => {
+                        return () => { }
+                    }}>
+                    <li className="nav-item" style={{ width: '15%', paddingRight: 10 }}>
+                        <fieldset className="border" style={{ padding: '10px', height: '100%' }}>
+                            <legend className="w-auto" style={{ fontSize: 'large' }}>Actions:</legend>
+                            <form>
+                                <button className="btn btn-primary" onClick={(event) => { setCarrier({ Name: '', Transform: '', ID: 0 }); setShowModal('New'); event.preventDefault() }}>
+                                    Add Cell Carrier
+                                </button>
+                            </form>
+                        </fieldset>
+                    </li>
+                </SearchBar>
+
+                <div style={{ width: '100%', height: 'calc( 100% - 136px)' }}>
+                    <Table<ICellCarrier>
+                        cols={[
+                            { key: 'Name', field: 'Name', label: 'Name', headerStyle: { width: '50%' }, rowStyle: { width: '50%' } },
+                            { key: 'Transform', field: 'Transform', label: 'Transform', headerStyle: { width: '50%' }, rowStyle: { width: '50%' } },
+                             { key: 'scroll', label: '', headerStyle: { width: 17, padding: 0 }, rowStyle: { width: 0, padding: 0 } },
+                        ]}
+                        tableClass="table table-hover"
+                        data={data}
+                        sortKey={sortField}
+                        ascending={asc}
+                        onSort={(d) => {
+                            if (d.colKey === 'scroll' || d.colKey === 'undefined')
+                                return
+                            if (d.colField === sortField)
+                                dispatch(CellCarrierSlice.Sort({ SortField: sortField, Ascending: asc }));
+                            else
+                                dispatch(CellCarrierSlice.Sort({ SortField: d.colField, Ascending: true }));
+                        }}
+                        onClick={(item) => { setCarrier(item.row); setShowModal('Edit'); }}
+                        theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                        tbodyStyle={{ display: 'block', overflowY: 'scroll', maxHeight: window.innerHeight - 300, width: '100%' }}
+                        rowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                        selected={() => false}
+                    />
+                </div>
+            </div>
+            <Modal Show={showModal != 'Hide'} ShowCancel={showModal == 'Edit'} CancelText={'Delete'} ShowX={true} Size='lg' Title={''} ConfirmText={showModal == 'Edit' ? 'Save' : 'Add'}
+                DisableConfirm={carrier.Name == null || carrier.Transform == null || carrier.Name.length == 0 || carrier.Transform.length == 0 || allData.findIndex(c => c.Name == carrier.Name && c.ID != carrier.ID) > -1}
+                ConfirmShowToolTip={carrier.Name == null || carrier.Transform == null || carrier.Name.length == 0 || carrier.Transform.length == 0 || allData.findIndex(c => c.Name == carrier.Name && c.ID != carrier.ID) > -1}
+                ConfirmToolTipContent={<>
+                    {carrier.Name == null || carrier.Name.length == 0 ? <p> {CrossMark} A Name is required. </p> : null}
+                    {carrier.Transform == null || carrier.Transform.length == 0 ? <p> {CrossMark} A Transform is required. </p> : null}
+                    {allData.findIndex(c => c.Name == carrier.Name && c.ID != carrier.ID) > -1 ? <p> {CrossMark} The Name has to be unique. </p> : null}
+                </>}
+                ConfirmBtnClass={'btn-success'} CallBack={(c, b) => {
+                    if (showModal == 'New' && c)
+                        CellCarrierSlice.DBAction({ verb: 'POST', record: carrier })
+                    if (showModal == 'Edit' && c)
+                        CellCarrierSlice.DBAction({ verb: 'PATCH', record: carrier })
+                    if (showModal == 'New' && b && !c)
+                        CellCarrierSlice.DBAction({ verb: 'DELETE', record: carrier })
+
+                    setShowModal('Hide');
+
+                }}
+            >
+                <div className="row">
+                    <div className="col">
+                        <Input<ICellCarrier> Record={carrier} Field={'Name'} Label={'Name'}
+                            Valid={() => allData.findIndex(c => c.Name == carrier.Name && c.ID != carrier.ID) < 0 && carrier.Name != null && carrier.Name.length > 0}
+                            Setter={(record) => setCarrier(record)} />
+                        <Input<ICellCarrier> Record={carrier} Field={'Transform'} Label={'Transform'} Help={'\'{0}\' will be replaced with the users Phone number.'}
+                            Valid={() => carrier.Transform != null && carrier.Transform.length > 0}
+                        Setter={(record) => setCarrier(record)} />
+                    </div>
+                </div>
+            </Modal>
+        </>)
+}
+
+export default ByCellCarrier;
--- Source/Applications/SystemCenterNotification/Scripts/TSX/SystemCenterNotification.tsx
+++ Source/Applications/SystemCenterNotification/Scripts/TSX/SystemCenterNotification.tsx
@@ -37,10 +37,14 @@ import BySubscription from './Subscriptions/Event/BySubscription';
 import { UserInfoSlice } from './Store';
 import ByAllSubscription from './Subscriptions/Event/ByAllSubscription';
 import EmailConfirmed from './EventSubscription/EmailConfirmed';
+import ByCellCarrier from './CellCarrier/ByCellCarrier';
 
 declare var homePath;
 declare var version;
 
+const Phone = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file">
+   <path d="m16.73442,2.78512l-9.40284,0c-0.29313,0 -0.53158,0.23844 -0.53158,0.53158l0,17.28407c0,0.29313 0.23848,0.53158 0.53158,0.53158l9.40284,0c0.29313,0 0.53158,-0.23848 0.53158,-0.53158l0,-17.28407c0,-0.29313 -0.23876,-0.53158 -0.53158,-0.53158zm-1.11059,0.89185c0.15403,0 0.27917,0.12515 0.27917,0.27948c0,0.154 -0.12513,0.27917 -0.27917,0.27917c-0.15428,0 -0.27945,-0.12513 -0.27945,-0.27917c-0.00003,-0.154 0.12545,-0.27948 0.27945,-0.27948zm-0.56653,0.15187c0.0705,0 0.12761,0.05711 0.12761,0.12761c0,0.07046 -0.05711,0.12758 -0.12761,0.12758c-0.07078,0 -0.12761,-0.05711 -0.12761,-0.12758c0,-0.0705 0.05711,-0.12761 0.12761,-0.12761zm-0.33749,0c0.0705,0 0.12761,0.05711 0.12761,0.12761c0,0.07046 -0.05711,0.12758 -0.12761,0.12758s-0.12758,-0.05711 -0.12758,-0.12758c-0.00032,-0.0705 0.0568,-0.12761 0.12758,-0.12761zm-3.73451,-0.15187l2.09509,0l0,0.55861l-2.09509,0l0,-0.55861zm2.09506,16.63767l-2.09509,0l0,-0.55864l2.09509,0l0,0.55864zm3.07656,-1.226l-8.24819,0l0,-14.20085l8.24819,0l0,14.20085z" stroke="null"/>
+</svg>
 
 
 const MainPage = (props: {}) => {
@@ -87,6 +91,9 @@ const MainPage = (props: {}) => {
                 </Page>
                 <Page Name='EventEmail/:id' >
                     <EmailPage useParams={{ id: '1' }} />
+                </Page>
+                <Page Name='Carrier' Label={'Cell Carrier'} Icon={Phone}>
+                    <ByCellCarrier />
                 </Page>
             </Section>
             <Page Name='ConfirmEmail' >
