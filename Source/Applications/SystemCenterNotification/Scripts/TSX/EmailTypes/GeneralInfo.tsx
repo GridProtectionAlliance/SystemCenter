56f1cfc41850542478e292e19e0cce98862e013a
Christoph Lackner <clackner@gridprotectionalliance.org>
2022-08-22 10:50:44 -0400

    Fixed Warning Symbol not showing up properly in tooltip

--- Source/Applications/SystemCenterNotification/Scripts/TSX/EmailTypes/GeneralInfo.tsx
+++ Source/Applications/SystemCenterNotification/Scripts/TSX/EmailTypes/GeneralInfo.tsx
@@ -23,8 +23,8 @@
 
 import { useAppDispatch, useAppSelector } from '../hooks';
 import * as React from 'react';
-import {ToolTip, Warning } from '@gpa-gemstone/react-interactive'
-import { CrossMark } from '@gpa-gemstone/gpa-symbols';
+import {ToolTip } from '@gpa-gemstone/react-interactive'
+import { CrossMark, Warning } from '@gpa-gemstone/gpa-symbols';
 import {  EmailType } from '../global';
 import {  EmailTypeSlice } from '../Store';
 import { IsNumber } from '@gpa-gemstone/helper-functions';
