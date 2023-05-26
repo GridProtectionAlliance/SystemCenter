76c607a48a7b2e4f17a1f2b931bd346e0ec339b6
Christoph Lackner <clackner@gridprotectionalliance.org>
2023-05-26 16:51:15 -0400

    Fixed Typing

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ApplicationManagment/ApplicationNode.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ApplicationManagment/ApplicationNode.tsx
@@ -73,7 +73,7 @@ const ByApplicationNode: Application.Types.iByComponent = (props) => {
         setErrors(e)
     }, [editnewNode])
 
-    const searchFields: Search.IField<SystemCenter.Types.Setting>[] = [
+    const searchFields: Search.IField<Application.Types.iApplicationNode>[] = [
         { key: 'Name', label: 'Application Name', type: 'string', isPivotField: false },
         { key: 'ID', label: 'Node ID', type: 'string', isPivotField: false }
     ]
@@ -87,7 +87,7 @@ const ByApplicationNode: Application.Types.iByComponent = (props) => {
         <>
             <LoadingScreen Show={status === 'loading'} />
             <div style={{ width: '100%', height: '100%' }}>
-                <SearchBar<SystemCenter.Types.Setting> CollumnList={searchFields} SetFilter={(flds) => dispatch(ApplicationNodeSlice.DBSearch({ filter: flds, sortField, ascending }))}
+                <SearchBar<Application.Types.iApplicationNode> CollumnList={searchFields} SetFilter={(flds) => dispatch(ApplicationNodeSlice.DBSearch({ filter: flds, sortField, ascending }))}
                     Direction={'left'} defaultCollumn={{ key: 'Name', label: 'Application Name', type: 'string', isPivotField: false }} Width={'50%'} Label={'Search'}
                     ShowLoading={searchStatus === 'loading'} ResultNote={searchStatus === 'error' ? 'Could not complete Search' : 'Found ' + data.length + ' Applications'}
                     GetEnum={() => {
