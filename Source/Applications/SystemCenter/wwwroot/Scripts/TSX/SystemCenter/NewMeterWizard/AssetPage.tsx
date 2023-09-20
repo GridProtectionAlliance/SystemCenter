c75f12ca2b4da27e34295d822e47c6cf5362583f
Ariana Armstrong <aarmstrong@gridprotectionalliance.org>
2023-09-20 14:29:52 -0400

    Declared a list promises. Returned a Promise for each of the selected assets. Assigned assetRecord to a Promise that will return the values of the assets' attributes and their additional fields. Pushed assetRecord into promises. Resolved promises into a single Promise, then once the Promise is resolved props is updated with list.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/NewMeterWizard/AssetPage.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/NewMeterWizard/AssetPage.tsx
@@ -38,6 +38,8 @@ import { Modal, Search } from '@gpa-gemstone/react-interactive';
 import DERAttributes from '../AssetAttribute/DER';
 import AssetSelect from '../Asset/AssetSelect';
 import { CrossMark, Pencil, TrashCan } from '@gpa-gemstone/gpa-symbols';
+import { getAssetWithAdditionalFields } from '../../../TS/Services/Asset';
+
 
 declare var homePath: string;
 
@@ -327,12 +329,11 @@ export default function AssetPage(props: IProps) {
                                 index = assetConnections.findIndex(assetConnection => assetConnection.Parent == asset.AssetKey || assetConnection.Child == asset.AssetKey);
                             }
                         });
-
-                        //Convert assets from slice to correct typing
-                        $.each(selected, (index, record) => {
-                            let assetRecord = { ...assets.find((asset) => asset.ID === record.ID), AssetType: record.AssetType as OpenXDA.Types.AssetTypeName, Channels: channels.filter((channel) => channel.Asset == record.AssetKey) };
-                            //Push converted asset to list
-                            list.push(assetRecord);
+                        let promises = [];
+                        $.each(selected, async (index, record) => {
+                            let assetRecord = getAssetWithAdditionalFields(record.ID, record.AssetType as OpenXDA.Types.AssetTypeName);
+                            // Push promises into promises
+                            promises.push(assetRecord);
                         });
 
                         //Add the new assets that have been created by the user
@@ -342,9 +343,9 @@ export default function AssetPage(props: IProps) {
 
                         //Update selected
                         setSelectedAssets(selected);
+                        Promise.all(promises).then(d => { props.UpdateAssets(list.concat(d)) })
 
                         //Update props
-                        props.UpdateAssets(list);
                         props.UpdateChannels(channels);
                         props.UpdateAssetConnections(assetConnections);
                     }}>
