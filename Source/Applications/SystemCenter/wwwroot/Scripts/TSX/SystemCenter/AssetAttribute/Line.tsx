717cee4c423b60e2a103c408532d79fb50e361de
Christoph Lackner <clackner@gridprotectionalliance.org>
2023-10-02 18:36:44 -0400

    Added Helper message

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetAttribute/Line.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AssetAttribute/Line.tsx
@@ -62,8 +62,8 @@ function LineAttributes(props: { NewEdit: Application.Types.NewEdit, Asset: Open
     if (props.Asset == null || props.Asset.Detail == null) return null;
     return (
         <>
-            <Input<OpenXDA.Types.Line> Record={props.Asset} Field={'MaxFaultDistance'} Label={'Max Fault Distance'} Feedback={'Max Fault Distance must be a numeric value.'} Valid={valid} Setter={props.UpdateState} Disabled={props.NewEdit == 'New' && props.Asset.ID != 0} />
-            <Input<OpenXDA.Types.Line> Record={props.Asset} Field={'MinFaultDistance'} Label={'Min Fault Distance'} Feedback={'Min Fault Distance must be a numeric value.'} Valid={valid} Setter={props.UpdateState} Disabled={props.NewEdit == 'New' && props.Asset.ID != 0} />
+            <Input<OpenXDA.Types.Line> Record={props.Asset} Help={'If this field is left blank the system will use Length as the maximum fault distance.'} Field={'MaxFaultDistance'} Label={'Max Fault Distance'} Feedback={'Max Fault Distance must be a numeric value.'} Valid={valid} Setter={props.UpdateState} Disabled={props.NewEdit == 'New' && props.Asset.ID != 0} />
+            <Input<OpenXDA.Types.Line> Record={props.Asset} Help={'If this field is left blank the system will use 0 as the minimum fault distance.'} Field={'MinFaultDistance'} Label={'Min Fault Distance'} Feedback={'Min Fault Distance must be a numeric value.'} Valid={valid} Setter={props.UpdateState} Disabled={props.NewEdit == 'New' && props.Asset.ID != 0} />
             <div className="alert alert-info" role="alert">
                 <p>Reactance, Length, and Thermal Rating are based on the Line Segments associated with this Line.</p>
                 <p>To change these values, edit the properties of the Line Segements.</p>
