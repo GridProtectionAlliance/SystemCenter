ff541430a683e2db6964beb5c1acf05030cdbc71
elwills <elwills@gridprotectionalliance.org>
2023-10-10 12:19:34 -0400

    Change modal title.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Location/LocationDrawings.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Location/LocationDrawings.tsx
@@ -135,7 +135,7 @@ const LocationDrawingsWindow = (props: { Location: OpenXDA.Types.Location }) => 
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title">Add New List Item</h5>
+                            <h5 className="modal-title">Add New Drawing</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
