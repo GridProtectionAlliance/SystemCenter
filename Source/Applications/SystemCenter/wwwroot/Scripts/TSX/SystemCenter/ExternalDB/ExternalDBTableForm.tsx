02d269edd03695e048f953f98f1644c5021558a1
pdinsdale <pdinsdale1128@gmail.com>
2023-10-12 22:21:19 -0400

    Increase size of query textbox

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/ExternalDBTableForm.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/ExternalDBTableForm.tsx
@@ -45,7 +45,7 @@ export default function ExternalDBTableForm(props: IProps) {
     return (
         <>
             <Input<SystemCenter.Types.extDBTables> Record={props.Record} Field={'TableName'} Label='Name' Feedback={'A Name of less than 200 characters is required.'} Valid={Valid} Setter={props.Setter} />
-            <TextArea<SystemCenter.Types.extDBTables> Rows={3} Record={props.Record} Field={'Query'} Valid={Valid} Setter={props.Setter} />
+            <TextArea<SystemCenter.Types.extDBTables> Rows={8} Record={props.Record} Field={'Query'} Valid={Valid} Setter={props.Setter} />
         </>
     );
 }
