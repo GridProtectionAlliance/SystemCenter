6b8a5142201e483e7b5352389f406ecba0ecc1b4
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2023-11-13 10:09:57 -0500

    Fix to bug where all addl fields were appearing in background

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/ExternalDBTableFields.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ExternalDB/ExternalDBTableFields.tsx
@@ -22,42 +22,48 @@
 //******************************************************************************************************
 
 import * as React from 'react';
-import { SystemCenter } from '@gpa-gemstone/application-typings';
+import * as _ from 'lodash';
+import { SystemCenter, Application } from '@gpa-gemstone/application-typings';
 import { useAppSelector, useAppDispatch } from '../hooks';
 import { AdditionalFieldsSlice, ValueListGroupSlice } from '../Store/Store';
 import ExternalDBTableFieldForm from './ExternalDBTableFieldForm';
 import Table from '@gpa-gemstone/react-table';
 import { CrossMark, Pencil, TrashCan, HeavyCheckMark, Warning } from '@gpa-gemstone/gpa-symbols';
-import { Modal, SearchBar } from '@gpa-gemstone/react-interactive';
+import { LoadingScreen, Modal, SearchBar, ServerErrorIcon } from '@gpa-gemstone/react-interactive';
 import { SelectPopup } from '@gpa-gemstone/common-pages';
+import { SearchStatus } from '../Store/AssetSlice';
+
+const emptyRecord: SystemCenter.Types.AdditionalField = {
+    ID: 0,
+    ParentTable: '',
+    FieldName: '',
+    Type: 'string',
+    ExternalDBTableID: 0,
+    IsSecure: false,
+    Searchable: false,
+    IsInfo: false,
+    IsKey: false,
+};
 
 export default function ExternalDBTableFields(props: { TableName: string, ID: number }) {
 
     const dispatch = useAppDispatch();
 
     const data = useAppSelector(AdditionalFieldsSlice.Data);
-    const searchData = useAppSelector(AdditionalFieldsSlice.SearchResults);
-    const sortKey = useAppSelector(AdditionalFieldsSlice.SortField);
-    const asc = useAppSelector(AdditionalFieldsSlice.Ascending);
     const status = useAppSelector(AdditionalFieldsSlice.Status);
-    const parentID = useAppSelector(AdditionalFieldsSlice.ParentID);
+    const searchData = useAppSelector(AdditionalFieldsSlice.SearchResults);
+    const searchStatus = useAppSelector(AdditionalFieldsSlice.SearchStatus);
 
     const valueListGroupData = useAppSelector(ValueListGroupSlice.Data);
     const valueListGroupStatus = useAppSelector(ValueListGroupSlice.Status);
 
-    const emptyRecord: SystemCenter.Types.AdditionalField = {
-        ID: 0,
-        ParentTable: '',
-        FieldName: '',
-        Type: 'string',
-        ExternalDBTableID: 0,
-        IsSecure: false,
-        Searchable: false,
-        IsInfo: false,
-        IsKey: false,
-    };
+    const [fieldsInTable, setFieldsInTable] = React.useState<SystemCenter.Types.AdditionalField[]>([]);
+    const parentID = React.useRef<number>(-1);
+    const [tableStatus, setTableStatus] = React.useState<Application.Types.Status>('unintiated');
+    const [asc, setAsc] = React.useState<boolean>(false);
+    const [sortKey, setSortKey] = React.useState<string>('FieldName');
+
     const [record, setRecord] = React.useState<SystemCenter.Types.AdditionalField>(emptyRecord);
-    const [origFields, setOrigFields] = React.useState<SystemCenter.Types.AdditionalField[]>(data);
     const [showRemove, setShowRemove] = React.useState<boolean>(false);
     const [showNew, setShowNew] = React.useState<boolean>(false);
     const [showExisting, setShowExisting] = React.useState<boolean>(false);
@@ -65,17 +71,40 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
     const [warnings, setWarnings] = React.useState<string[]>([]);
 
     React.useEffect(() => {
-        if (!showExisting && !showNew && (status == 'unintiated' || status == 'changed' || parentID !== props.ID))
-            dispatch(AdditionalFieldsSlice.Fetch(props.ID));
-    }, [showExisting, showNew, status, parentID, props.ID, parentID]);
+        if (status !== 'idle') return;
+        if (tableStatus === 'unintiated' || tableStatus === 'changed' || parentID.current !== props.ID) {
+            setTableStatus('loading');
+            parentID.current = props.ID;
+            const handle = $.ajax({
+                type: "GET",
+                url: `${homePath}api/SystemCenter/AdditionalField/${parentID.current}/${sortKey}/${asc ? '1' : '0'}`,
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json',
+                cache: false,
+                async: true
+            });
+            handle.done((results) => {
+                console.log(JSON.parse(results.toString()))
+                sortData(JSON.parse(results.toString()));
+                setTableStatus('idle');
+            });
+            handle.fail(() => {
+                setTableStatus('error');
+            });
+        }
+    }, [tableStatus, props.ID, status]);
 
     React.useEffect(() => {
-        if (showExisting || showNew)
+        sortData(fieldsInTable);
+    }, [sortKey, asc]);
+
+    React.useEffect(() => {
+        if (status === 'unintiated' || status === 'changed')
             dispatch(AdditionalFieldsSlice.Fetch());
-    }, [showExisting, showNew]);
+    }, [status]);
 
     React.useEffect(() => {
-        if (valueListGroupStatus == 'unintiated' || valueListGroupStatus == 'changed')
+        if (valueListGroupStatus === 'unintiated' || valueListGroupStatus === 'changed')
             dispatch(ValueListGroupSlice.Fetch());
     }, [valueListGroupStatus]);
 
@@ -100,6 +129,10 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
 
         setWarnings(w);
     }, [record, data]);
+
+    const sortData = React.useCallback((sortData: SystemCenter.Types.AdditionalField[]) => {
+        setFieldsInTable(_.orderBy(sortData, [sortKey], [(!asc ? "asc" : "desc")]));
+    }, [setFieldsInTable, sortKey, asc]);
 
     function Delete() {
         dispatch(AdditionalFieldsSlice.DBAction({ verb: 'DELETE', record: { ...record } }));
@@ -127,64 +160,68 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
             <div className="card-body">
                 <div className="row">
                     <div style={{ width: '100%', height: window.innerHeight - 421, maxHeight: window.innerHeight - 421, padding: 0, overflowY: 'auto' }}>
-                        <Table<SystemCenter.Types.AdditionalField>
-                            cols={[
-                                { key: 'FieldName', field: 'FieldName', label: 'Name', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-                                { key: 'ParentTable', field: 'ParentTable', label: 'Parent Type', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-                                { key: 'Type', field: 'Type', label: 'Field Type', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-                                {
-                                    key: 'Searchable', label: 'Searchable', field: 'Searchable', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                                    content: (item) => item.Searchable ? HeavyCheckMark : CrossMark
-                                },
-                                {
-                                    key: 'IsSecure', label: 'Secure', field: 'IsSecure', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                                    content: (item) => item.IsSecure ? HeavyCheckMark : CrossMark
-                                },
-                                {
-                                    key: 'IsInfo', label: 'Info', field: 'IsInfo', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                                    content: (item) => item.IsInfo ? HeavyCheckMark : CrossMark
-                                },
-                                {
-                                    key: 'IsKey', label: 'Key', field: 'IsKey', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                                    content: (item) => item.IsKey ? HeavyCheckMark : CrossMark
-                                },
-                                {
-                                    key: 'btns', field: 'ID', label: '', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                                    content: (item) => <>
-                                        <button className="btn btn-sm" onClick={(e) => {
-                                            e.preventDefault();
-                                            setRecord(item);
-                                            setShowNew(true);
-                                        }}>{Pencil}</button>
-                                        <button className="btn btn-sm" onClick={(e) => {
-                                            e.preventDefault();
-                                            setRecord(item);
-                                            setShowRemove(true);
-                                        }}>{TrashCan}</button>
-                                    </>
-                                },
-                                { key: 'scroll', label: '', headerStyle: { width: 17, padding: 0 }, rowStyle: { width: 0, padding: 0 } },
-                            ]}
-                            tableClass="table table-hover"
-                            data={data}
-                            sortKey={sortKey}
-                            ascending={asc}
-                            onSort={(d) => {
-                                if (d.colKey == 'btns' || d.colKey == 'scroll' || d.colField == null) return;
-                                dispatch(AdditionalFieldsSlice.Sort({ SortField: d.colField, Ascending: !asc }));
-                            }}
-                            theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                            tbodyStyle={{ display: 'block', maxHeight: window.innerHeight - 455, }}
-                            rowStyle={{ display: 'table', tableLayout: 'fixed', width: '100%' }}
-                            selected={() => false}
-                        />
+                        <LoadingScreen Show={tableStatus === 'loading'} />
+                        {tableStatus === 'error' ?
+                            <ServerErrorIcon Show={true} Label={'A Server Error Occurred. Please Reload the Application.'} /> :
+                            <Table<SystemCenter.Types.AdditionalField>
+                                cols={[
+                                    { key: 'FieldName', field: 'FieldName', label: 'Name', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                                    { key: 'ParentTable', field: 'ParentTable', label: 'Parent Type', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                                    { key: 'Type', field: 'Type', label: 'Field Type', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
+                                    {
+                                        key: 'Searchable', label: 'Searchable', field: 'Searchable', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
+                                        content: (item) => item.Searchable ? HeavyCheckMark : CrossMark
+                                    },
+                                    {
+                                        key: 'IsSecure', label: 'Secure', field: 'IsSecure', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
+                                        content: (item) => item.IsSecure ? HeavyCheckMark : CrossMark
+                                    },
+                                    {
+                                        key: 'IsInfo', label: 'Info', field: 'IsInfo', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
+                                        content: (item) => item.IsInfo ? HeavyCheckMark : CrossMark
+                                    },
+                                    {
+                                        key: 'IsKey', label: 'Key', field: 'IsKey', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
+                                        content: (item) => item.IsKey ? HeavyCheckMark : CrossMark
+                                    },
+                                    {
+                                        key: 'btns', field: 'ID', label: '', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
+                                        content: (item) => <>
+                                            <button className="btn btn-sm" onClick={(e) => {
+                                                e.preventDefault();
+                                                setRecord(item);
+                                                setShowNew(true);
+                                            }}>{Pencil}</button>
+                                            <button className="btn btn-sm" onClick={(e) => {
+                                                e.preventDefault();
+                                                setRecord(item);
+                                                setShowRemove(true);
+                                            }}>{TrashCan}</button>
+                                        </>
+                                    },
+                                    { key: 'scroll', label: '', headerStyle: { width: 17, padding: 0 }, rowStyle: { width: 0, padding: 0 } },
+                                ]}
+                                tableClass="table table-hover"
+                                data={fieldsInTable}
+                                sortKey={sortKey}
+                                ascending={asc}
+                                onSort={(d) => {
+                                    if (d.colKey == 'btns' || d.colKey == 'scroll' || d.colField == null) return;
+                                    if (d.colKey === sortKey) setAsc(prev => !prev);
+                                    else setSortKey(d.colKey);
+                                }}
+                                theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                tbodyStyle={{ display: 'block', maxHeight: window.innerHeight - 455, }}
+                                rowStyle={{ display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                selected={() => false}
+                            />}
                     </div>
                 </div>
             </div>
             <div className="card-footer">
                 <div className="btn-group mr-2">
                     <button className="btn btn-primary"
-                        onClick={() => { setOrigFields(data); setShowExisting(true); }}
+                        onClick={() => { setShowExisting(true); }}
                     >Add Existing Field</button>
                 </div>
                 <div className="btn-group mr-2">
@@ -195,8 +232,8 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
             </div>
 
             <Modal Title={'Remove External DB Table Field'} Show={showRemove} CallBack={() => setShowRemove(false)} ShowCancel={false} ConfirmText={'Close'} >
-                <button className="btn btn-danger btn-block" onClick={() => { DisassociateField(record); setShowRemove(false); }}>Disassociate Field From This Table</button>
-                <button className="btn btn-danger btn-block" onClick={() => { Delete(); setShowRemove(false); }}>Delete Field Permanently</button>
+                <button className="btn btn-danger btn-block" onClick={() => { setTableStatus('changed'); DisassociateField(record); setShowRemove(false); }}>Disassociate Field From This Table</button>
+                <button className="btn btn-danger btn-block" onClick={() => { setTableStatus('changed'); Delete(); setShowRemove(false); }}>Delete Field Permanently</button>
             </Modal>
 
             <Modal Title={record.ID == 0 ? 'Add New Field' : 'Edit ' + (record?.FieldName ?? 'Field')} Show={showNew} ShowCancel={false} ConfirmText={record.ID == 0 ? 'Add' : 'Save'}
@@ -210,10 +247,11 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
                 DisableConfirm={errors.length > 0}
                 ShowX={true} CallBack={(conf) => {
                     setShowNew(false);
-                    if (conf && record.ID > 0)
-                        dispatch(AdditionalFieldsSlice.DBAction({ verb: 'PATCH', record }));
-                    else if (conf && record.ID == 0)
-                        dispatch(AdditionalFieldsSlice.DBAction({ verb: 'POST', record }));
+                    if (conf) {
+                        setTableStatus('changed');
+                        if (record.ID > 0) dispatch(AdditionalFieldsSlice.DBAction({ verb: 'PATCH', record }));
+                        else if (record.ID == 0) dispatch(AdditionalFieldsSlice.DBAction({ verb: 'POST', record }));
+                    }
                 }}
             >
                 <ExternalDBTableFieldForm Record={record} Setter={setRecord} SetErrors={setErrors} />
@@ -223,13 +261,13 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
             <SelectPopup<SystemCenter.Types.AdditionalField>
                 Slice={AdditionalFieldsSlice}
                 Title={"Add Fields to " + (props.TableName ?? 'External DB Table')}
-                Selection={origFields}
+                Selection={fieldsInTable}
                 OnClose={(selected, conf) => {
                     setShowExisting(false);
-
                     if (conf) {
-                        selected.filter((s) => origFields.findIndex((o) => o.ID === s.ID) < 0).forEach((f) => AssociateField(f));
-                        origFields.filter((o) => selected.findIndex((s) => s.ID === o.ID) < 0).forEach((f) => DisassociateField(f));
+                        setTableStatus('changed');
+                        selected.filter((s) => fieldsInTable.findIndex((o) => o.ID === s.ID) < 0).forEach((f) => AssociateField(f));
+                        fieldsInTable.filter((o) => selected.findIndex((s) => s.ID === o.ID) < 0).forEach((f) => DisassociateField(f));
                     }
                 }}
                 Show={showExisting}
@@ -273,8 +311,8 @@ export default function ExternalDBTableFields(props: { TableName: string, ID: nu
                         defaultCollumn={{ label: 'Name', key: 'FieldName', type: 'string', isPivotField: false }}
                         Width={'50%'}
                         Label={'Search'}
-                        ShowLoading={status == 'loading'}
-                        ResultNote={status == 'error' ? 'Could not complete Search' : 'Found ' + searchData.length + ' Additional Field(s)'}
+                        ShowLoading={searchStatus == 'loading'}
+                        ResultNote={searchStatus == 'error' ? 'Could not complete Search' : 'Found ' + searchData.length + ' Additional Field(s)'}
                     >
                         {children}
                     </SearchBar>
