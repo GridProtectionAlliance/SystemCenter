fb60721bf85791010f14d4f301cac64612856447
Parker Dinsdale <pdinsdale1128@gmail.com>
2023-12-26 09:36:27 -0500

    Update event tag and event type tables

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/EventType/ByEventType.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/EventType/ByEventType.tsx
@@ -22,7 +22,7 @@
 //******************************************************************************************************
 
 import * as React from 'react';
-import Table from '@gpa-gemstone/react-table'
+import { ReactTable } from '@gpa-gemstone/react-table'
 import * as _ from 'lodash';
 import { useHistory } from "react-router-dom";
 import { Application, OpenXDA } from '@gpa-gemstone/application-typings';
@@ -87,28 +87,12 @@ const ByEventType: Application.Types.iByComponent = (props) => {
                 </nav>
             </div>            
             <div style={{ width: '100%', height: 'calc( 100% - 136px)' }}>
-                <Table<OpenXDA.Types.EventType>
-                    cols={[
-                        { key: 'Name', field: 'Name', label: 'Name', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-                       
-                        { key: 'Category', field: 'Category', label: 'Category', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-                        { key: 'Description', field: 'Description', label: 'Description', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' } },
-
-                        {
-                            key: 'ShowInFilter', field: 'ShowInFilter', label: 'Show in UI', headerStyle: { width: 'auto' }, rowStyle: { width: 'auto' },
-                            content: (item) => item.ShowInFilter ? HeavyCheckMark : CrossMark
-                        },
-
-                        { key: 'Scroll', label: '', headerStyle: { width: 17, padding: 0 }, rowStyle: { width: 0, padding: 0 } },
-                    ]}
-                    tableClass="table table-hover"
-                    data={eventTypes}
-                    sortKey={sortKey}
-                    ascending={ascending}
-                    onSort={(d) => {
-                        if (d.colKey === "Scroll")
-                            return;
-
+                <ReactTable.Table<OpenXDA.Types.EventType>
+                    TableClass="table table-hover"
+                    Data={eventTypes}
+                    SortKey={sortKey}
+                    Ascending={ascending}
+                    OnSort={(d) => {
                         if (d.colKey === sortKey)
                             setAscending(!ascending);
                         else {
@@ -116,12 +100,47 @@ const ByEventType: Application.Types.iByComponent = (props) => {
                             setSortKey(d.colField);
                         }
                     }}
-                    onClick={(item) => setSelected(item.row)}
-                    theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    tbodyStyle={{ display: 'block', overflowY: 'scroll', maxHeight: window.innerHeight - 300, width: '100%'  }}
-                    rowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    selected={(item) => false}
-                />
+                    OnClick={(item) => setSelected(item.row) }
+                    TheadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                    TbodyStyle={{ display: 'block', overflowY: 'scroll', maxHeight: window.innerHeight - 300, width: '100%' }}
+                    RowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                    Selected={(item) => false}
+                    KeySelector={(item) => item.ID}
+                >
+                    <ReactTable.Column<OpenXDA.Types.EventType>
+                        Key={'Name'}
+                        AllowSort={true}
+                        Field={'Name'}
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    > Name
+                    </ReactTable.Column>
+                    <ReactTable.Column<OpenXDA.Types.EventType>
+                        Key={'Category'}
+                        AllowSort={true}
+                        Field={'Category'}
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    > Category
+                    </ReactTable.Column>
+                    <ReactTable.Column<OpenXDA.Types.EventType>
+                        Key={'Description'}
+                        AllowSort={true}
+                        Field={'Description'}
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    > Description
+                    </ReactTable.Column>
+                    <ReactTable.Column<OpenXDA.Types.EventType>
+                        Key={'ShowInFilter'}
+                        AllowSort={true}
+                        Field={'ShowInFilter'}
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                        Content={({ item }) => item.ShowInFilter ? HeavyCheckMark : CrossMark }
+                    > Show in UI
+                    </ReactTable.Column>
+                </ReactTable.Table>
             </div>
 
             <Modal Show={selected != null} Title={'Edit ' + (selected?.Name ?? 'Event Type')}
