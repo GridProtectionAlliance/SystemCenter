4bafabc5e7b769bad666faa8e511a40e1f8b2407
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-03-11 13:18:37 -0400

    Removed 2nd button from Warning if no option to proceed

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ValueListGroup/ValueListGroupDelete.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ValueListGroup/ValueListGroupDelete.tsx
@@ -32,22 +32,26 @@ export const requiredValueLists = ["TimeZones","Make","Model","Unit","Category"]
 
 export function ValueListGroupDelete(props: IProps) {
     const [message, setMessage] = React.useState<string>('')
-    const [prevent, setPrevent]  = React.useState<boolean>(false)
-  
+    const [prevent, setPrevent] = React.useState<boolean>(false)
+    const [showCancel, setShowCancel] = React.useState<boolean>(true);
+
     React.useEffect(() => {
         if (requiredValueLists.includes(props.Record?.Name)) {
             setPrevent(true);
             setMessage('This Value List Group is required and cannot be removed.')
+            setShowCancel(false)
             return
         }
+        setShowCancel(true)
         setPrevent(false);
         setMessage('This will permanently delete this Value List Group and cannot be undone.')
     }, [props.Record])
 
-    return ( <Warning
-                Message={message}
-                Show={props.Show} Title={'Delete ' + (props.Record?.Name ?? 'Value List Group')}
-                CallBack={(c) => {props.CallBack(c && !prevent)}} />
+    return (<Warning
+        Message={message}
+        Show={props.Show} Title={'Delete ' + (props.Record?.Name ?? 'Value List Group')}
+        ShowCancel={showCancel}
+        CallBack={(c) => {props.CallBack(c && !prevent)}} />
     )
 }
 
@@ -64,6 +68,7 @@ export function ValueListItemDelete(props: IPropsItem) {
     const [message, setMessage] = React.useState<string>('')
     const [prevent, setPrevent]  = React.useState<boolean>(false)
     const [removalCount, setRemovalCount] = React.useState<number>(0);
+    const [showCancel, setShowCancel] = React.useState<boolean>(true);
 
     React.useEffect(() => {
         if ((props.Group?.Name?.length ?? 0) === 0 || (props.Record?.Value ?? 0) === 0)
@@ -84,31 +89,37 @@ export function ValueListItemDelete(props: IPropsItem) {
     React.useEffect(() => {
         if (requiredValueLists.includes(props.Group?.Name) && props.ItemCount == 1) {
             setPrevent(true);
+            setShowCancel(false);
             setMessage('This Value List Group is required and must contain at least 1 item.')
         }
         else if (props.ItemCount == 1 && removalCount > 0)
         {
             setMessage('Removing this Value List Item will result in an empty Value List Group. All Fields using this Value List Group will be changed to strings.')
             setPrevent(false);
+            setShowCancel(true);
         }
         else if (requiredValueLists.includes(props.Group?.Name) && removalCount > 0) {
             setPrevent(true);
+            setShowCancel(false);
             setMessage('This Value List Group is required and this Value List Item is still in use. Use of this Value List Item must be removed before it can be deleted.')
         }
         else if (removalCount > 0)
         {
             setMessage(`This Value List Group is in use, with ${removalCount} values corresponding to this Value List Item. These values will be unassigned.`)
+            setShowCancel(false);
             setPrevent(false);
         }
         else {
             setPrevent(false);
+            setShowCancel(true);
             setMessage('This will permanently delete this Value List Item and cannot be undone.')   
         }
     }, [props.Group, removalCount, props.ItemCount])
 
     return ( <Warning
-                Message={message}
-                Show={props.Show} Title={'Delete ' + (props.Record.AltValue ?? props.Record.Value)}
-                CallBack={(c) => {props.CallBack(c && !prevent)}} />
+        Message={message}
+        Show={props.Show} Title={'Delete ' + (props.Record.AltValue ?? props.Record.Value)}
+        ShowCancel={showCancel}
+        CallBack={(c) => {props.CallBack(c && !prevent)}} />
     )
 }
