76f04f4d9a9ef65c494a1ad197baf6a7e7bfbc3d
Kulindu Cooray <kcooray@gridprotectionalliance.org>
2024-06-03 17:07:33 -0400

    SC-107 removed margins

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ValueListGroup/ValueListGroupItem.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ValueListGroup/ValueListGroupItem.tsx
@@ -61,7 +61,7 @@ export default function ValueListGroupItems(props: IProps) {
     }
 
     return (
-        <div className="card" style={{ flex: 1, display: 'flex', flexDirection: 'column', marginBottom: 10, overflow: 'hidden' }}>
+        <div className="card" style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
             <div className="card-header">
                 <div className="row">
                     <div className="col">
