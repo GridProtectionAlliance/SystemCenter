2df14237fe55ba8b1c8494e7dd880e10868f644c
Erika Wills <elwills@gridprotectionalliance.org>
2024-07-01 12:40:44 -0400

    SC-142 Hide fieldset for Viewer on Assets page.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Asset/ByAsset.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Asset/ByAsset.tsx
@@ -276,7 +276,7 @@ const ByAsset: Application.Types.iByComponent = (props) => {
                 ResultNote={pageState === 'error' ? 'Could not complete Search' :
                     ('Displaying Transmission Asset(s) ' + (pageInfo.TotalRecords > 0 ? (pageInfo.RecordsPerPage * page + 1) : 0) + ' - ' + (pageInfo.RecordsPerPage * page + data.length)) + ' out of ' + pageInfo.TotalRecords}
             >
-                <li className="nav-item" style={{ width: '15%', paddingRight: 10 }}>
+                <li className="nav-item" hidden={props.Roles.indexOf('Administrator') < 0 && props.Roles.indexOf('Engineer') < 0} style={{ width: '15%', paddingRight: 10 }}>
                     <fieldset className="border" style={{ padding: '10px', height: '100%' }}>
                         <legend className="w-auto" style={{ fontSize: 'large' }}>Actions:</legend>
                         <form>
