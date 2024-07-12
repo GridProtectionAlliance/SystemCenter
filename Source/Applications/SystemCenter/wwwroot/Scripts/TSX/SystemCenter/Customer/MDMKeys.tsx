8f5fa81c572d343a3fcf4b5917ce178af4f9139c
Erika Wills <elwills@gridprotectionalliance.org>
2024-07-12 16:43:35 -0400

    SC-143 Update Transmission SME to Engineer in Customer pages.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Customer/MDMKeys.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Customer/MDMKeys.tsx
@@ -80,7 +80,7 @@ function MDMKeys(props: IProps) {
     }
 
     function hasPermissions(): boolean {
-        if (roles.indexOf('Administrator') < 0 && roles.indexOf('Transmission SME') < 0)
+        if (roles.indexOf('Administrator') < 0 && roles.indexOf('Engineer') < 0)
             return false;
         return true;
     }
