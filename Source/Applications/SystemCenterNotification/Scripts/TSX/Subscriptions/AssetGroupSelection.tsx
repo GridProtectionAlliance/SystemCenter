a963dae53392df354014899d56c7c1edaef83e2d
collins-self <cself@GridProtectionAlliance.org>
2024-07-18 10:15:11 -0400

    Changed to col's added row div

--- Source/Applications/SystemCenterNotification/Scripts/TSX/Subscriptions/AssetGroupSelection.tsx
+++ Source/Applications/SystemCenterNotification/Scripts/TSX/Subscriptions/AssetGroupSelection.tsx
@@ -130,9 +130,9 @@ const AssetGroupSelection = (props: IProps) => {
     return (<>
             <LoadingIcon Show={parentGroupState == 'loading' || assetGrpStatus == 'loading'} />
             <div className="col">
-                <div className="row">
-                    <div className="col">
-                        <Select<OpenXDA.Types.AssetGroup> Record={selectedParent} Field={'ID'} Label='Asset Group' Setter={setSelectedParent}
+                <div className="col">
+                    <div className="form-group">
+                        <Select<OpenXDA.Types.AssetGroup> Record={selectedParent} Field={'ID'} Label='Asset Category' Setter={setSelectedParent}
                             Options={[{ Label: 'Other', Value: '-1' }].concat(parentGroups.map((p) => {
                                 return { Label: p.Name, Value: p.ID.toString() }
                             }))} />
