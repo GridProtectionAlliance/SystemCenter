3550012e1d2a3028b9963a8ed5d59962df9a6b45
Erika Wills <elwills@gridprotectionalliance.org>
2024-07-22 16:16:57 -0400

    Remove unnecessary hidden attribute.

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/EventTag/ByEventTag.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/EventTag/ByEventTag.tsx
@@ -104,7 +104,7 @@ const EventTags: Application.Types.iByComponent = (props) => {
                     <fieldset className="border" style={{ padding: '10px', height: '100%' }}>
                         <legend className="w-auto" style={{ fontSize: 'large' }}>Actions:</legend>
                         <form>
-                            <button className="btn btn-primary" hidden={props.Roles.indexOf('Administrator') < 0 && props.Roles.indexOf('Transmission SME') < 0}
+                            <button className="btn btn-primary" 
                                 onClick={(evt) => { evt.preventDefault(); setRecord({ ...emptyRecord }); setMode('Add'); }}>Add Tag</button>
                         </form>
                     </fieldset>
