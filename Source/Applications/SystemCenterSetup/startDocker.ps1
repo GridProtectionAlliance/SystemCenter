3716ac6cc6387acbb09a780b3b1aa6da01a9bee5
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-08-15 14:10:05 -0400

    Updated Docker Container File

--- Source/Applications/SystemCenterSetup/startDocker.ps1
+++ Source/Applications/SystemCenterSetup/startDocker.ps1
@@ -2,20 +2,12 @@
 [Environment]::SetEnvironmentVariable("CompanyName", "$env:CompanyName", "Machine")
 [Environment]::SetEnvironmentVariable("CompanyAcronym", "$env:CompanyAcronym", "Machine")
 [Environment]::SetEnvironmentVariable("ConnectionString", "$env:ConnectionString", "Machine")
-
-Set-Content "C:/Program Files/SystemCenter/SystemCenter.exe.config" $(Get-Content "C:/Program Files/SystemCenter/SystemCenter.exe.config").Replace("env(USER:ConnectionString)", "$env:ConnectionString")
+[Environment]::SetEnvironmentVariable("NodeID", "$env:NodeID", "Machine")
 
 
 Write-Host "Starting SystemCenter"
 $ServiceName = 'SystemCenter'; 
-
-
 Restart-Service $ServiceName
+Write-Host "Started SystemCenter"
 
-$lastCheck = (Get-Date).AddSeconds(-2) 
-while ($true) 
-{ 
-    Get-EventLog -LogName Application -Source "SystemCenter" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message	 
-    $lastCheck = Get-Date 
-    Start-Sleep -Seconds 2 
-}
+get-content 'C:/Program Files/SystemCenter/SystemCenter.StatusLog.txt' -tail 1 -wait
