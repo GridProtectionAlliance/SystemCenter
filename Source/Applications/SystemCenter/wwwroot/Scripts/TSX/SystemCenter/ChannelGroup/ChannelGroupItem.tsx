3f92363e7757bb6f3cc334652d3ab1262f334b44
Kulindu Cooray <kcooray@gridprotectionalliance.org>
2024-08-15 20:08:55 -0400

    SC-194 changed button layout

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ChannelGroup/ChannelGroupItem.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/ChannelGroup/ChannelGroupItem.tsx
@@ -168,7 +168,7 @@ export default function ChannelGroupDetails(props: IProps) {
             </div>
             <div className="card-footer">
                 <div className="btn-group mr-2">
-                    <button className="btn btn-primary pull-right"
+                    <button className="btn btn-info pull-right"
                         onClick={() => { setRecord({ ...emptyRecord, ChannelGroupID: props.Record.ID }); setShowModal(true); }}
                     >Add Item</button>
                 </div>
