e272fe44d55d3d1f430c8450a92771a3bb1ceabf
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-10-30 10:17:39 -0400

    Migrated ByCustomer Page

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Customer/ByCustomer.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Customer/ByCustomer.tsx
@@ -22,238 +22,107 @@
 //******************************************************************************************************
 
 import * as React from 'react';
-import { Paging, ReactTable } from '@gpa-gemstone/react-table';
 import * as _ from 'lodash';
 import { useHistory } from "react-router-dom";
-import { Application, OpenXDA, SystemCenter } from '@gpa-gemstone/application-typings';
-import { DefaultSearchField, SearchFields } from '../CommonComponents/SearchFields';
-import { SearchBar, Search, Modal, GenericController } from '@gpa-gemstone/react-interactive';
-import { useAppSelector, useAppDispatch } from '../hooks';
+import { Application, OpenXDA } from '@gpa-gemstone/application-typings';
+import { Modal, GenericController } from '@gpa-gemstone/react-interactive';
 import CustomerForm from './CustomerForm';
-import { CrossMark, HeavyCheckMark } from '@gpa-gemstone/gpa-symbols';
 import ExternalDBUpdate from '../CommonComponents/ExternalDBUpdate';
-import { CustomerSlice } from '../Store/Store';
-
+import GenericByPage from '../CommonComponents/GenericByPage';
+import { SystemCenter as SC } from '../global';
+import { ReactIcons } from '@gpa-gemstone/gpa-symbols';
 
 declare var homePath: string;
+
+const fieldCols: SC.IByCol<OpenXDA.Types.Customer>[] = [
+    { Field: 'Name', Label: 'Name', Type: 'string', Width: '15%' },
+    { Field: 'CustomerKey', Label: 'Customer Key', Type: 'string', Width: '15%' },
+    { Field: 'Phone', Label: 'Phone', Type: 'string', Width: '10%' },
+    { Field: 'Description', Label: 'Description', Type: 'string', Width: 'auto' },
+    { Field: 'LSCVS', Label: 'LSCVS', Type: 'boolean', Width: 'auto' }
+];
+
+const controllerPath = `${homePath}api/SystemCenter/Customer`;
+
+
 const CustomerController = new GenericController<OpenXDA.Types.Customer>(`${homePath}api/SystemCenter/Customer`, "CustomerKey", false);
 const emptyCustomer: OpenXDA.Types.Customer = { ID: 0, CustomerKey: '', Name: '', Phone: '', Description: '', LSCVS: false, PQIFacilityID: -1 };
 
 const ByCustomer: Application.Types.iByComponent = (props) => {
-    let dispatch = useAppDispatch();
     let history = useHistory();
+    const extDbUpdateAll = React.useRef<() => (() => void)>(undefined);
 
-    
-    const [allPages, setAllPages] = React.useState<number>(1);
-    const [page, setPage] = React.useState<number>(1);
-    const [data, setData] = React.useState<OpenXDA.Types.Customer[]>([]);
-    const [state, setState] = React.useState<Application.Types.Status>('idle');
-    const [update, forceUpdate] = React.useState<boolean>(false);
-    
-    const [filterableList, setFilterableList] = React.useState<Array<Search.IField<OpenXDA.Types.Customer>>>(SearchFields.Customer as Search.IField<OpenXDA.Types.Customer>[]);
-    
-    const [sortKey, setSortKey] = React.useState<keyof OpenXDA.Types.Customer>('Name');
-    const [search, setSearch] = React.useState<Search.IFilter<OpenXDA.Types.Customer>[]>([]);
-    const [ascending, setAscending] = React.useState<boolean>(true);
     const [newCustomer, setNewCustomer] = React.useState<OpenXDA.Types.Customer>(emptyCustomer);
 
     const [showModal, setShowModal] = React.useState<boolean>(false);
     const [errors, setErrors] = React.useState<string[]>([]);
-
     const [showEXTModal, setShowExtModal] = React.useState<boolean>(false);
-    const extDbUpdateAll = React.useRef<() => (() => void)>(undefined);
-
-    React.useEffect(() => {
-        setState('loading')
-        const h = CustomerController.PagedSearch(search, sortKey, ascending, page);
-        h.then((d) => {
-            setData(d.Data);
-            setAllPages(d.TotalRecords);
-            setState('idle');
-        }, () => setState('error'));
-        return function () {
-            if (h != null && h.abort != null) h.abort();
-        }
-    }, [search, ascending, sortKey, page, update]);
-
-    React.useEffect(() => {
-        let handle = getAdditionalFields();
-
-        return () => {
-            if (handle.abort != null) handle.abort();
-        }
-    }, []);
-
-    function getAdditionalFields(): JQuery.jqXHR<Array<SystemCenter.Types.AdditionalField>> {
-        let handle = $.ajax({
-            type: "GET",
-            url: `${homePath}api/SystemCenter/AdditionalFieldView/ParentTable/Customer/FieldName/0`,
-            contentType: "application/json; charset=utf-8",
-            cache: false,
-            async: true
-        });
-
-        function ConvertType(type: string) {
-            if (type == 'string' || type == 'integer' || type == 'number' || type == 'datetime' || type == 'boolean')
-                return { type: type }
-            return {
-                type: 'enum', enum: [{ Label: type, Value: type }]
-            }
-        }
-
-        handle.done((d: Array<SystemCenter.Types.AdditionalFieldView>) => {
-            let ordered = _.orderBy((SearchFields.Customer as Search.IField<OpenXDA.Types.Customer>[]).concat(d.filter(item => item.Searchable).map(item => (
-                { label: `[AF${item.ExternalDB != undefined ? " " + item.ExternalDB : ''}] ${item.FieldName}`, key: item.FieldName, ...ConvertType(item.Type) } as Search.IField<OpenXDA.Types.Customer>
-            ))), ['label'], ["asc"]);
-            setFilterableList(ordered)
-        });
-
-        return handle;
-    }
-
+    const [refreshCount, refreshData] = React.useState<number>(0);
     function handleSelect(item) {
         history.push({ pathname: homePath + 'index.cshtml', search: '?name=Customer&CustomerID=' + item.row.ID })
     }
 
     return (
-        <div className="container-fluid d-flex h-100 flex-column">
-            <SearchBar<OpenXDA.Types.Customer> CollumnList={filterableList} SetFilter={(flds) => setSearch(flds)} Direction={'left'} defaultCollumn={DefaultSearchField.Customer as Search.IField<OpenXDA.Types.Customer>} Width={'50%'} Label={'Search'} StorageID="CustomersFilter"
-                ShowLoading={state == 'loading'} ResultNote={state == 'error' ? 'Could not complete Search' : 'Found ' + data.length + ' Customer(s)'}
-                GetEnum={(setOptions, field) => {
-                    let handle = null;
-                   
-                    if (field.type != 'enum' || field.enum == undefined || field.enum.length != 1)
-                        return () => { };
-
-                    handle = $.ajax({
-                        type: "GET",
-                        url: `${homePath}api/ValueList/Group/${field.enum[0].Value}`,
-                        contentType: "application/json; charset=utf-8",
-                        dataType: 'json',
-                        cache: true,
-                        async: true
-                    });
-
-                    handle.done(d => setOptions(d.map(item => ({ Value: item.Value.toString(), Label: item.Text }))))
-                    return () => { if (handle != null && handle.abort == null) handle.abort(); }
-                }}
-
-            >
-                <li className="nav-item" hidden={props.Roles.indexOf('Administrator') < 0 && props.Roles.indexOf('Engineer') < 0} style={{ width: '15%', paddingRight: 10 }}>
-                    <fieldset className="border" style={{ padding: '10px', height: '100%' }}>
-                        <legend className="w-auto" style={{ fontSize: 'large' }}>Actions:</legend>
-                        <div className="form-group">
-                            <button className="btn btn-primary" onClick={(event) => {
-                                event.preventDefault();
-                                setNewCustomer(emptyCustomer);
-                                setShowModal(true);
-                            }}>Add Customer</button>
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-primary" 
-                                onClick={(event) => { event.preventDefault(); setShowExtModal(true); }}>External Database</button>
-                        </div>
-                    </fieldset>
-                </li>
-            </SearchBar>
-            <div className="row" style={{ flex: 1, overflow: 'hidden' }}>
-                <ReactTable.Table<OpenXDA.Types.Customer>
-                    TableClass="table table-hover"
-                    Data={data}
-                    SortKey={sortKey}
-                    Ascending={ascending}
-                    OnSort={(d) => {
-                        if (d.colKey === sortKey)
-                            setAscending(!ascending);
-                        else {
-                            setAscending(true);
-                            setSortKey(d.colField);
-                        }
-                    }}
-                    OnClick={handleSelect}
-                    TableStyle={{
-                        padding: 0, width: '100%', height: '100%',
-                        tableLayout: 'fixed', overflow: 'hidden', display: 'flex', flexDirection: 'column', marginBottom: 0
-                    }}
-                    TheadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    TbodyStyle={{ display: 'block', overflowY: 'auto', flex: 1 }}
-                    RowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    Selected={(item) => false}
-                    KeySelector={(item) => item.ID}
-                >
-                    <ReactTable.Column<OpenXDA.Types.Customer>
-                        Key={'Name'}
-                        AllowSort={true}
-                        Field={'Name'}
-                        HeaderStyle={{ width: '15%' }}
-                        RowStyle={{ width: '15%' }}
-                    > Name
-                    </ReactTable.Column>
-                    <ReactTable.Column<OpenXDA.Types.Customer>
-                        Key={'CustomerKey'}
-                        AllowSort={true}
-                        Field={'CustomerKey'}
-                        HeaderStyle={{ width: '15%' }}
-                        RowStyle={{ width: '15%' }}
-                    > Customer Key
-                    </ReactTable.Column>
-                    <ReactTable.Column<OpenXDA.Types.Customer>
-                        Key={'Phone'}
-                        AllowSort={true}
-                        Field={'Phone'}
-                        HeaderStyle={{ width: '10%' }}
-                        RowStyle={{ width: '10%' }}
-                    > Phone
-                    </ReactTable.Column>
-                    <ReactTable.Column<OpenXDA.Types.Customer>
-                        Key={'Description'}
-                        AllowSort={true}
-                        Field={'Description'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto' }}
-                    > Description
-                    </ReactTable.Column>
-                    <ReactTable.Column<OpenXDA.Types.Customer>
-                        Key={'LSCVS'}
-                        AllowSort={true}
-                        Field={'LSCVS'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto' }}
-                        Content={({ item }) => {
-                            if (item.LSCVS == true)
-                                return HeavyCheckMark;
-                            if (item.LSCVS == false)
-                                return null;
-                        }}
-                    > LSCVS
-                    </ReactTable.Column>
-                </ReactTable.Table>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <Paging Current={page + 1} Total={allPages} SetPage={(p) => setPage(p - 1)} />
-                </div>
-            </div>
+        <GenericByPage<OpenXDA.Types.Customer>
+            ControllerPath={controllerPath}
+            RefreshData={refreshCount}
+            DefaultSortKey='Name'
+            PagingID='ByCustomer'
+            OnClick={(item) => { handleSelect(item); }}
+            Columns={fieldCols}
+            DefaultSearchAscending={false} DefaultSearchKey='Name'
+            QueryAdditionalFields={() => 
+                $.ajax({
+                    type: "GET",
+                    url: `${homePath}api/SystemCenter/AdditionalFieldView/ParentTable/Customer/FieldName/0`,
+                    contentType: "application/json; charset=utf-8",
+                    cache: false,
+                    async: true
+                }) }
+        >
+            <li className="nav-item" hidden={props.Roles.indexOf('Administrator') < 0 && props.Roles.indexOf('Engineer') < 0} style={{ width: '15%', paddingRight: 10 }}>
+                <fieldset className="border" style={{ padding: '10px', height: '100%' }}>
+                    <legend className="w-auto" style={{ fontSize: 'large' }}>Actions:</legend>
+                    <div className="form-group">
+                        <button className="btn btn-primary" onClick={(event) => {
+                            event.preventDefault();
+                            setNewCustomer(emptyCustomer);
+                            setShowModal(true);
+                        }}>Add Customer</button>
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary"
+                            onClick={(event) => { event.preventDefault(); setShowExtModal(true); }}>External Database</button>
+                    </div>
+                </fieldset>
+            </li>
             <Modal Show={showModal} Title={'Add New Customer'} CallBack={(c) => {
                 setShowModal(false);
-                if (!c) 
+                if (!c)
                     return;
-                forceUpdate(x => !x);
-                CustomerController.DBAction( 'POST', newCustomer );
-                
-            }} ShowCancel={false} ShowX={true} DisableConfirm={errors.length > 0} ConfirmShowToolTip={errors.length > 0} ConfirmToolTipContent={errors.map((t, i) => <p key={i}> {CrossMark} {t}</p>)} >
+                CustomerController.DBAction('POST', newCustomer).then(() => refreshData(x => x + 1))
+            }}
+                ShowCancel={false}
+                ShowX={true}
+                DisableConfirm={errors.length > 0}
+                ConfirmShowToolTip={errors.length > 0}
+                ConfirmToolTipContent={errors.map((t, i) => <p key={i}> <ReactIcons.CrossMark Color='var(--danger)'/> {t}</p>)} >
                 <div className="row">
                     <CustomerForm Customer={newCustomer} stateSetter={setNewCustomer} setErrors={setErrors} />
                 </div>
             </Modal>
-            <Modal Show={showEXTModal} Size={'xlg'} Title={'Customer External Database Fields'}
-                ShowCancel={true} ConfirmText={'Update All'} CancelText={'Close'} CallBack={(c) => {
+            <Modal Show={showEXTModal} Size={'xlg'}
+                Title={'Customer External Database Fields'}
+                ShowCancel={false}
+                ShowX={true}
+                ConfirmText={'Update All'}
+                CallBack={(c) => {
                     if (c && extDbUpdateAll.current !== undefined) extDbUpdateAll.current();
                     if (!c) setShowExtModal(false);
                 }}>
                 <ExternalDBUpdate Type='Customer' UpdateAll={extDbUpdateAll} />
             </Modal>
-        </div>
+        </GenericByPage>
     )
 }
 
