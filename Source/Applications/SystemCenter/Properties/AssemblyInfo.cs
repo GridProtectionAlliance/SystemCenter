043314a47c8933044e9b44474a33f982e59cc942
gsfbuildbot <support@gridprotectionalliance.org>
2024-11-26 00:46:45 -0500

    SystemCenter: Version change for build v3.0.495.69-master.

--- Source/Applications/SystemCenter/Properties/AssemblyInfo.cs
+++ Source/Applications/SystemCenter/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.494.69")]
-[assembly: AssemblyVersion("3.0.494.69")]
-[assembly: AssemblyFileVersion("3.0.494.69")]
+// [assembly: AssemblyVersion("3.0.495.69")]
+[assembly: AssemblyVersion("3.0.495.69")]
+[assembly: AssemblyFileVersion("3.0.495.69")]
