d6ab48cccab4588b011e6adf0ca96c72f518088e
collins-self <cself@GridProtectionAlliance.org>
2025-01-17 09:01:35 -0500

    enhance LocationDrawingsButton with dropdown options and error handling

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/CommonComponents/LocationDrawingsButton.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/CommonComponents/LocationDrawingsButton.tsx
@@ -30,17 +30,27 @@ interface LocationDrawingsButtonProps {
     Locations: OpenXDA.Types.Location[];
 }
 
+type DropDownOption = {
+    Label: JSX.Element | string,
+    Callback: () => void,
+    Disabled: boolean
+    ToolTipContent: JSX.Element,
+    ShowToolTip: boolean,
+    ToolTipLocation: ('top' | 'bottom' | 'left' | 'right'),
+    Key: string | number
+}
+
 const LocationDrawingsButton: React.FC<LocationDrawingsButtonProps> = (props) => {
     const [hover, setHover] = React.useState<'none' | 'drawings'>('none');
     const [pageState, setPageState] = React.useState<"loading" | "error" | "idle">("idle");
     const [selectedLocation, setSelectedLocation] = React.useState<OpenXDA.Types.Location>();
     const multipleLocations = React.useMemo(() => props.Locations.length > 1, [props.Locations]);
     const [showDrawingsModal, setShowDrawingsModal] = React.useState<boolean>(false);
-    const [locationsWithErrors, setLocationsWithErrors] = React.useState<Map<number, string[]>>(new Map())
+    const [locationOptions, setLocationOptions] = React.useState<DropDownOption[]>([]);
     const LocationDrawingController = new GenericController(`${homePath}api/LocationDrawing`, "Name", true);
 
     const isValid = (location: OpenXDA.Types.Location, drawingData) => {
-        let e = [];
+        let e = "";
 
         if (!location
             || (location.Alias == ""
@@ -50,21 +60,31 @@ const LocationDrawingsButton: React.FC<LocationDrawingsButtonProps> = (props) =>
                 && location.LocationKey == ""
                 && location.Longitude == null
                 && location.Name == ""))
-            e.push('No locations have been set.');
+            e = 'No locations have been set.';
         else if (drawingData.TotalRecords == 0)
-            e.push('No drawings associated with location.');
+            e = 'No drawings associated with location.';
         return e;
     }
 
     React.useEffect(() => { // Generates the map of errors for each location
         setPageState('loading');
 
-        const handles = props.Locations.map(location => {
+        const handles = props.Locations.map((location, i) => {
+            setLocationOptions([]);
             if (location != null) {
                 const handle = LocationDrawingController.PagedSearch([], 'Name', true, 1, location.ID)
                     .done((result) => {
-                        const errors = isValid(location, result);
-                        updateLocationErrors(location, errors);
+                        const error = isValid(location, result);
+                        const option: DropDownOption = {
+                            Label: location.Name,
+                            Callback: () => handleShowDrawingsModal(location),
+                            Disabled: error != "",
+                            ToolTipContent: <p>{error}</p>,
+                            ShowToolTip: error != "",
+                            ToolTipLocation: "left",
+                            Key: i
+                        };
+                        setLocationOptions(prev => [...prev, option]);
                     })
                     .fail(() => { throw new Error() });
                 return handle;
@@ -87,40 +107,6 @@ const LocationDrawingsButton: React.FC<LocationDrawingsButtonProps> = (props) =>
         };
     }, [props.Locations]);
 
-    const handleAddLocationError = (locMap: Map<number, string[]>) => {
-        setLocationsWithErrors(prev => {
-            const newMap = new Map(prev);
-            locMap.forEach((errors, locID) => {
-                if (newMap.has(locID)) {  // If the location already has errors
-                    const existingErrors = newMap.get(locID);
-                    newMap.set(locID, Array.from(new Set([...existingErrors, ...errors]))); // supresses duplicate values
-                } else { // otherwise add new
-                    newMap.set(locID, errors);
-                }
-            });
-            return newMap;
-        });
-    }
-
-    const handleRemoveLocationError = (locID: number) => {
-        setLocationsWithErrors(prev => {
-            const newMap = new Map(prev);
-            newMap.delete(locID);
-            return newMap;
-        });
-    }
-
-    const updateLocationErrors = (loc: OpenXDA.Types.Location, errors: string[]) => {
-        if (locationsWithErrors.has(loc?.ID)                             // Remove if the location has
-            && locationsWithErrors.get(loc?.ID).length > errors.length) {// fewer errors than before
-            handleRemoveLocationError(loc.ID);
-        } else if (errors.length > 0) {
-            const locationErrorsMap = new Map<number, string[]>();
-            locationErrorsMap.set(loc?.ID, errors);
-            handleAddLocationError(locationErrorsMap);
-        }
-    }
-
     const handleShowDrawingsModal = (loc) => {
         if (loc == undefined) return;
         setSelectedLocation(loc);
@@ -134,42 +120,32 @@ const LocationDrawingsButton: React.FC<LocationDrawingsButtonProps> = (props) =>
             {!multipleLocations
             ? <>
             <button
-                className={locationsWithErrors.has(props.Locations[0]?.ID) ? "btn btn-primary disabled" : "btn btn-primary"}
-                onClick={() => locationsWithErrors.has(props.Locations[0]?.ID) ? null : handleShowDrawingsModal(props.Locations[0])}
+                className={locationOptions[0]?.Disabled ? "btn btn-primary disabled" : "btn btn-primary"}
+                onClick={() => locationOptions[0]?.Disabled ? null : handleShowDrawingsModal(props.Locations[0])}
                 data-tooltip={"DrawingsModal"}
                 onMouseEnter={() => setHover('drawings')}
                 onMouseLeave={() => setHover('none')}
                 >Open {props.Locations[0]?.Name} Drawings
             </button>
             <ToolTip
-                Show={locationsWithErrors.has(props.Locations[0]?.ID) && hover === 'drawings'}
+                Show={locationOptions[0]?.Disabled && hover === 'drawings'}
                 Theme={'dark'}
                 Position={'top'}
                 Target={"DrawingsModal"}
                 Zindex={9999}
-                > {locationsWithErrors.get(props.Locations[0]?.ID)?.map((e, i) => <p key={i}>{CrossMark} {e}</p>)}
+                    ><p>{CrossMark} {locationOptions[0]?.ToolTipContent}</p>
             </ToolTip>
             </>
             : <BtnDropdown
                 Label={'Open ' + props.Locations[0]?.Name + ' Drawings'}
                 Callback={() => handleShowDrawingsModal(props.Locations[0])}
                 TooltipContent={
-                    <>{locationsWithErrors.get(props.Locations[0]?.ID)?.map((e, i) => <p key={i}>{CrossMark} {e}</p>)}</>
+                    <p>{CrossMark} {locationOptions[0]?.Disabled}</p>
                 }
-                ShowToolTip={locationsWithErrors.has(props.Locations[0]?.ID)}
-                Disabled={locationsWithErrors.has(props.Locations[0]?.ID)}
+                ShowToolTip={locationOptions[0]?.ShowToolTip}
+                Disabled={locationOptions[0]?.Disabled}
                 BtnClass={'btn-primary'}
-                Options={props.Locations.slice(1).map((loc, i) => ({
-                    Label: 'Open ' + loc?.Name + ' Drawings',
-                    Callback: () => handleShowDrawingsModal(loc),
-                    Disabled: locationsWithErrors.has(loc?.ID),
-                    ToolTipContent: <>{
-                        locationsWithErrors.get(loc?.ID)?.map((e, i) => <p key={i}>{CrossMark} {e}</p>)
-                    }</>,
-                    ShowToolTip: locationsWithErrors.has(loc?.ID),
-                    ToolTipLocation: "left",
-                    Key: i
-                }))}
+                Options={locationOptions.slice(1)}
             />
             }
             <LocationDrawingsModal
