23829e31fa1346f7955753858de70678105bdb36
Collins Self <cself@GridProtectionAlliance.org>
2025-03-13 14:32:47 -0400

    Update Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/DeviceIssuesPage/OpenXDAIssuesPage.tsx
    Co-authored-by: Erika Wills <elwills@gridprotectionalliance.org>
    Signed-off-by: Collins Self <cself@GridProtectionAlliance.org>

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/DeviceIssuesPage/OpenXDAIssuesPage.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/DeviceIssuesPage/OpenXDAIssuesPage.tsx
@@ -37,7 +37,7 @@ const OpenXDADailyStatisticController = new GenericController<SC.OpenXDADailySta
 function OpenXDAIssuesPage(props: { Meter: OpenXDA.Types.Meter }) {
     const [data, setData] = React.useState<SC.OpenXDADailyStatistic[]>([]);
     const [sortField, setSortField] = React.useState<keyof SC.OpenXDADailyStatistic>('Date');
-    const [ascending, setAscending] = React.useState<boolean>(true);
+    const [ascending, setAscending] = React.useState<boolean>(false);
 
     const order = React.useCallback((data: SC.OpenXDADailyStatistic[]) => {
         return _.orderBy(data, [sortField], [ascending ? 'asc' : 'desc'])
