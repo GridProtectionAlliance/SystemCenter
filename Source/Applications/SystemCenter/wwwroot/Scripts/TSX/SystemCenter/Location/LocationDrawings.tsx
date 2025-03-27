58d8ff67ec8bc43e1516f3382f69f95b9a887dba
Christina Gentry <CGentry@gridprotectionalliance.org>
2025-03-27 09:29:21 -0400

    Updated buttons in Location folder

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Location/LocationDrawings.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/Location/LocationDrawings.tsx
@@ -272,8 +272,8 @@ const LocationDrawingsWindow = (props: { Location: OpenXDA.Types.Location }) => 
 
                             </div>
                             <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => handleSave()}>Save changes</button>
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => handleSave()}>Save</button>
+                                <button type="button" className="btn btn-danger" data-dismiss="modal">Cancel</button>
                             </div>
                         </div>
                     </div>
