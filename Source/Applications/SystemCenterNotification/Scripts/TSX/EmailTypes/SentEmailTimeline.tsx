56cab75ed477f8ae0d861bcff70deb5b1659cea5
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-03-30 13:21:32 -0400

    add plot

--- Source/Applications/SystemCenterNotification/Scripts/TSX/EmailTypes/SentEmailTimeline.tsx
+++ Source/Applications/SystemCenterNotification/Scripts/TSX/EmailTypes/SentEmailTimeline.tsx
@@ -28,27 +28,37 @@ import { Application } from '@gpa-gemstone/application-typings'
 import { Table, Column } from '@gpa-gemstone/react-table'
 import moment from 'moment';
 import { Modal } from '@gpa-gemstone/react-interactive'
-import { Plot } from '@gpa-gemstone/react-graph'
+import { Pill, Plot, VerticalMarker } from '@gpa-gemstone/react-graph'
 
 interface TimelineItem {
-    Timestamp: string,
+    Start: string,
+    End?: string | null,
     Description: string,
     ID: number
 }
 
+interface Timestamp {
+    Description: string,
+    Timestamp: string
+    TimelineID: number,
+    ID: number
+}
 
 interface IProps {
     SentEmail: SentEmail
-    Show: boolean
-    CallBack: ((confirmed: boolean, isButton: boolean, tertiary: boolean) => void)
 }
 
 const SentEmailTimeline = (props: IProps) => {
-
-    const [emailTimeline, setEmailTimeline] = React.useState<TimelineItem[]>(null)
+    const [timeline, setTimeline] = React.useState<TimelineItem[]>(null)
     const [state, setState] = React.useState<Application.Types.Status>("uninitiated")
-    const [sortKey, setSortKey] = React.useState<keyof TimelineItem>('Timestamp')
+    const [sortKey, setSortKey] = React.useState<keyof Timestamp>('Timestamp')
     const [ascending, setAscending] = React.useState<boolean>(false)
+    const [plotHeight, setPlotHeight] = React.useState<number>(100);
+    const [plotWidth, setPlotWidth] = React.useState<number>(100);
+    const [selectedID, setSelectedID] = React.useState<number>(null);
+    const rowRef = React.useRef<HTMLDivElement>(null);
+
+    const [timeframe, setTimeframe] = React.useState<[number, number]>([null, null]) 
 
 
     React.useEffect(() => {
@@ -56,7 +66,7 @@ const SentEmailTimeline = (props: IProps) => {
         let handle = getTimeline();
         handle.done((dt: string) => {
             const eventResults = JSON.parse(dt) as TimelineItem[];
-            setEmailTimeline(eventResults);
+            setTimeline(eventResults);
             setState('idle');
         }).fail((d) => setState('error'));
 
@@ -65,6 +75,13 @@ const SentEmailTimeline = (props: IProps) => {
                 handle.abort();
         }
     }, [props.SentEmail, sortKey, ascending])
+
+    React.useEffect(() => {
+        const times = timeline?.map((i) => { return moment(i.Start).valueOf() })
+        const startTime = times != null ? Math.min(...times) : null
+        const endTime = times != null ? Math.max(...times) : null
+        setTimeframe([startTime, endTime])
+    }, [timeline])
 
 
     const getTimeline = () => {
@@ -75,60 +92,160 @@ const SentEmailTimeline = (props: IProps) => {
             dataType: 'json',
             cache: false,
             async: true,
-            data: JSON.stringify({"Ascending": ascending, "OrderBy": sortKey})
+            data: JSON.stringify({ "Ascending": ascending, "OrderBy": sortKey })
         });
     }
 
+    // set plot dimensions
+    React.useLayoutEffect(() => {
+        setPlotHeight(rowRef?.current?.offsetHeight ?? 100)
+        setPlotWidth((rowRef?.current?.offsetWidth ?? 130) - 30)
+    });
+
+    const handleTableOnClick = (data) => {
+        setSelectedID(data.row.TimelineID)
+        const timelineItem = timeline.find((item) => item.ID == data.row.TimelineID)
+        timelineItem.End == null ? setTimeframe(TenMinutesBeforeAndAfter(timelineItem))
+            : setTimeframe(ThreeTimesDurationBeforeAndAfter(timelineItem))
+    }
+
     return (
-        <Modal
-            Title={"Email Timeline"}
-            Show={props.Show}
-            CallBack={props.CallBack}
-            Size={'xlg' }
-        >
+        <>
             {state !== 'idle' ? <></> :
-                <Table<TimelineItem>
-                    TableClass="table table-hover"
-                    Data={emailTimeline}
-                    SortKey={sortKey.toString()}
-                    Ascending={ascending}
-                    OnSort={(d) => {
-                        if (d.colKey === sortKey.toString()) setAscending(ascending => !ascending);
-                        else setSortKey(d.colField);
-                    }}
+                <div className="container-fluid d-flex h-100 flex-column">
+                    <div className="row h-10" ref={rowRef}>
+                        <div className="col-12">
+                            
+                            <Plot
+                                defaultTdomain={timeframe}
+                                height={plotHeight}
+                                width={plotWidth}
+                                showGrid={true}
+                                XAxisType='time'
+                                legend='bottom'
+                                Ylabel=''
+                                Tlabel='Time'
+                                pan={false}
+                                zoom={false}
+                                holdMenuOpen={false}
+                                yDomain={'HalfAutoValue'}
+                                showDateOnTimeAxis={true}
+                                hideYAxis={true}
+                                
+                            >
+                                {timeline.map((item, i) => {
+                                    return (
+                                        item.End == null ?
+                                            <VerticalMarker
+                                                key={i}
+                                                Value={moment(item.Start).valueOf()}
+                                                color={selectedID == item.ID ? "yellow" : "black"}
+                                                width={3}
+                                                lineStyle={'solid'}
+                                            /> :
+
+                                            <Pill
+                                                key={i}
+                                                XData={[moment(item.Start).valueOf(), moment(item.End).valueOf()]}
+                                                YData={[1,2]}
+                                                RadiusPX={200}
+                                                Color={selectedID == item.ID ? "yellow" : "green"}
+                                                Text={item.Description}
+                                            />
+                                    )
+                                })
+                                }
+                            </Plot>
+                        </div>
+                    </div>
+                    <div className="row h-90">
+                        <div className="col-12">
+                            <Table<Timestamp>
+                                TableClass="table table-hover"
+                                Data={ToTimestamps(timeline)} 
+                                SortKey={sortKey.toString()}
+                                Ascending={ascending}
+                                OnSort={(d) => {
+                                    if (d.colKey === sortKey.toString()) setAscending(ascending => !ascending);
+                                    else setSortKey(d.colField);
+                                }}
                                 KeySelector={(item) => item.ID}
-                    TableStyle={{
-                        padding: 0, width: 'calc(100%)', height: 'calc(100% - 16px)',
-                        tableLayout: 'fixed', overflow: 'hidden', display: 'flex', flexDirection: 'column'
-                    }}
-                    TheadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    TbodyStyle={{ display: 'block', overflowY: 'scroll', flex: 1 }}
-                    RowStyle={{ display: 'table', tableLayout: 'fixed', width: '100%' }}
-                    Selected={(item) => false}
-                >
-                    <Column<TimelineItem>
-                        Key={'Description'}
-                        AllowSort={true}
-                        Field={'Description'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto' }}
-                    > Description
-                    </Column>
-                    <Column<TimelineItem>
-                        Key={'Timestamp'}
-                        AllowSort={true}
-                        Field={'Timestamp'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto' }}
-                        Content={({ item, field }) => {
-                            return <span className={`badge badge-pill badge-light`}>{moment(item[field]).format('MM/DD/YYYY HH:mm:ss')}</span>
-                        }}
-                    > Timestamp
-                    </Column>
-                </Table>
-            }
-        </Modal>
-    )
+                                TheadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                RowStyle={{ display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                Selected={(item) => false}
+                                OnClick={(data) => handleTableOnClick(data) }
+                            >
+                                <Column<Timestamp>
+                                    Key={'Description'}
+                                    AllowSort={true}
+                                    Field={'Description'}
+                                    HeaderStyle={{ width: 'auto' }}
+                                    RowStyle={{ width: 'auto' }}
+                                > Description
+                                </Column>
+                                <Column<Timestamp>
+                                    Key={'Timestamp'}
+                                    AllowSort={true}
+                                    Field={'Timestamp'}
+                                    HeaderStyle={{ width: 'auto' }}
+                                    RowStyle={{ width: 'auto' }}
+                                    Content={({ item, field }) => {
+                                        return <span className={`badge badge-pill badge-light`}>{moment(item[field]).format('MM/DD/YYYY HH:mm:ss')}</span>
+                                    }}
+                                > Timestamp
+                                </Column>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            }</>)
 }
 
 export default SentEmailTimeline;
+
+const ToTimestamps = (timeline: TimelineItem[]): Timestamp[] => {
+    let timestamps = []
+    let index = 0
+    timeline.forEach((i) => {
+        if (i.End == null) {
+            timestamps.push({
+                Description: i.Description,
+                Timestamp: i.Start,
+                TimelineID: i.ID,
+                ID: index
+            })
+            index++
+            return
+        }
+        timestamps.push({
+            Description: i.Description + ' Start',
+            Timestamp: i.Start,
+            TimelineID: i.ID,
+            ID: index
+        })
+        index++
+        timestamps.push({
+            Description: i.Description + ' End',
+            Timestamp: i.End,
+            TimelineID: i.ID,
+            ID: index
+        })
+        index++
+    })
+    return timestamps
+}
+
+const TenMinutesBeforeAndAfter = (item: TimelineItem): [number, number] => {
+    const newTimeframe = [moment(item.Start).valueOf() - 60000, moment(item.Start).valueOf() + 60000]
+    console.log(`start: ${moment(item.Start).valueOf()}, newframe: ${newTimeframe}`)
+    return newTimeframe as [number, number]
+}
+
+const ThreeTimesDurationBeforeAndAfter = (item: TimelineItem): [number, number] => {
+    const start = moment(item.Start).valueOf()
+    const end = moment(item.End).valueOf()
+    const duration = end - start
+    const newTimeframe = [start - duration * 3, end + duration * 3]
+    console.log(`start: ${start}, end: ${end}, duration: ${duration}, newframe: ${newTimeframe}`)
+    return newTimeframe as [number, number]
+}
