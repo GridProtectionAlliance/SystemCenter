361f337dc8ee76251070ec89634867414f1c083f
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-05-08 10:53:52 -0400

    split into separate components

---
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AppHost/FilesProcessedGraph.tsx
@@ -0,0 +1,112 @@
+﻿//******************************************************************************************************
+//  FilesProcessedGraph.tsx - Gbtc
+//
+//  Copyright © 2026, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/07/2026 - Natalie Beatty
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+import * as React from 'react'
+import moment from 'moment'
+import { Application } from '@gpa-gemstone/application-typings';
+import { Plot, Bar } from '@gpa-gemstone/react-graph'
+
+export interface IAggregateProcessedFile {
+    Hour: string,
+    Count: number
+}
+
+interface IProps {
+    OffsetHeight: number
+    OffsetWidth: number
+    FilteredHour: string
+    SelectedTime: string
+    SetFilteredHour: React.Dispatch<React.SetStateAction<string>>
+}
+
+const FilesProcessedGraph = (props: IProps) => {
+    const [yMax, setYMax] = React.useState<number>(0)
+    const [timeframe, setTimeframe] = React.useState<[number, number]>([moment().subtract(48, 'hour').startOf('hour').valueOf(), moment().add(1, 'hour').startOf('hour').valueOf()])
+    const [status, setStatus] = React.useState<Application.Types.Status>('uninitiated')
+    const [aggregateProcessedFiles, setAggregateProcessedFiles] = React.useState<IAggregateProcessedFile[]>([])
+
+    React.useEffect(() => {
+        getAggregateRecentlyProcessedFiles()
+    }, [])
+
+    function getAggregateRecentlyProcessedFiles() {
+        const h = $.ajax({
+            type: "GET",
+            url: `${homePath}api/OpenXDA/DataFile/AggregateRecentlyProcessedFiles`,
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            cache: false,
+            async: true,
+        })
+
+        h.done((d) => {
+            setAggregateProcessedFiles(d)
+            setYMax(Math.max(...d?.map((c) => { return c.Count })))
+        }).fail(() => {
+            setStatus('error')
+        })
+        return function cleanup() {
+            if (h.abort != null)
+                h.abort();
+
+        }
+    }
+
+    const handleOnPlotSelect = React.useCallback((x: number, y: number[]) => {
+        const selectedHour = aggregateProcessedFiles.find(a => moment(a.Hour).valueOf() < x && (moment(a.Hour).valueOf() + 3600000) > x && y[0] < a.Count)
+        props.SetFilteredHour(selectedHour?.Hour ?? null)
+    }, [aggregateProcessedFiles])
+
+    return <Plot
+        height={props.OffsetHeight}
+        width={props.OffsetWidth}
+        defaultTdomain={timeframe}
+        defaultYdomain={[0, yMax]}
+        onTDomainChange={setTimeframe}
+        zoom={false}
+        yZoom={false}
+        xZoom={false}
+        Tmin={timeframe[0]}
+        Tmax={timeframe[1]}
+        Ymin={0}
+        Ymax={yMax}
+        Ylabel={'Files Queued'}
+        onSelect={handleOnPlotSelect}
+        pan={false}
+        defaultMouseMode={'select'}
+    >
+        {aggregateProcessedFiles.length == 0 ? null :
+            aggregateProcessedFiles.map((a) => {
+                return <Bar
+                    Data={[a.Count]}
+                    BarOrigin={moment(a.Hour).valueOf()}
+                    BarWidth={3600000}
+                    Color={a.Hour === props.FilteredHour ? 'yellow' : moment(a.Hour).startOf('hour').valueOf() === moment(props.SelectedTime).startOf('hour').valueOf() && props.SelectedTime !== null ? 'green' : 'black'}
+                    key={a.Hour}
+                >
+                </Bar>
+            })}
+    </Plot>
+}
+
+export default FilesProcessedGraph
---
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AppHost/FilesProcessedTable.tsx
@@ -0,0 +1,187 @@
+﻿//******************************************************************************************************
+//  FilesProcessedTable.tsx - Gbtc
+//
+//  Copyright © 2026, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/07/2026 - Natalie Beatty
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+import * as React from 'react'
+import { Table, Paging, Column } from '@gpa-gemstone/react-table'
+import { SystemCenter as SC } from '../global';
+import moment from 'moment'
+import { Application } from '@gpa-gemstone/application-typings';
+import ProcessingStatus from '../CommonComponents/ProcessingStatus'
+
+interface IProps {
+    FilteredHour: string
+    SelectedFile: number
+    HandleOnTableClick: (data: any, evt: React.MouseEvent) => void
+}
+
+const FilesProcessedTable = (props: IProps) => {
+    const [sortField, setSortField] = React.useState<keyof SC.DataFile>('ID')
+    const [ascending, setAscending] = React.useState<boolean>(false)
+    const [dataFile, setDataFile] = React.useState<SC.DataFile[]>([])
+    const [totalPages, setTotalPages] = React.useState<number>(0)
+    const [page, setPage] = React.useState<number>(0);
+    const [status, setStatus] = React.useState<Application.Types.Status>('uninitiated')
+
+    React.useEffect(() => {
+        setStatus('loading')
+        getFileGroups()
+        setStatus('idle')
+    }, [sortField, ascending, page, props.FilteredHour])
+
+    function getFileGroups() {
+        let filters = []
+        if (props.FilteredHour === null) {
+            filters = [{
+                FieldName: 'ProcessingStartTime',
+                Operator: '>',
+                Type: 'datetime',
+                SearchText: moment().subtract(48, 'hour').startOf('hour').format('YYYY-MM-DD HH:mm:ss.SSS')
+            }]
+        }
+        else {
+            filters = [{
+                FieldName: 'ProcessingStartTime',
+                Operator: '>=',
+                Type: 'datetime',
+                SearchText: moment(props.FilteredHour).format('YYYY-MM-DD HH:mm:ss.SSS')
+            },
+            {
+                FieldName: 'ProcessingStartTime',
+                Operator: '<',
+                Type: 'datetime',
+                SearchText: moment(props.FilteredHour).add(1, 'hour').format('YYYY-MM-DD HH:mm:ss.SSS')
+            }
+            ]
+        }
+        const h = $.ajax({
+            type: "POST",
+            url: `${homePath}api/OpenXDA/DataFile/PagedList/${page}`,
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            cache: false,
+            async: true,
+            data: JSON.stringify({ Searches: filters, OrderBy: sortField, Ascending: ascending }),
+        });
+
+        h.done((d) => {
+            setDataFile(JSON.parse(d.Data))
+            setTotalPages(d.NumberOfPages)
+            if (page >= d.NumberOfPages && d.NumberOfPages > 0)
+                setPage(d.NumberOfPages - 1)
+            setStatus('idle')
+        }).fail(() => {
+            setStatus('error')
+        })
+
+        return function cleanup() {
+            if (h.abort != null)
+                h.abort();
+        }
+    }
+
+    return <>
+        <Table<SC.DataFile>
+            Data={dataFile}
+            SortKey={sortField}
+            Ascending={ascending}
+            KeySelector={(item) => item.ID}
+            OnSort={(d) => {
+                if (d.colField == sortField) {
+                    setAscending(!ascending);
+                }
+                else {
+                    setAscending(true);
+                    setSortField(d.colField);
+                }
+            }}
+            OnClick={props.HandleOnTableClick}
+            Selected={(item) => item.FileGroupID === props.SelectedFile}
+        >
+            <Column<SC.DataFile>
+                Key={'FileName'}
+                AllowSort={false}
+                Field={'FileName'}
+                HeaderStyle={{ width: 'auto' }}
+                RowStyle={{ width: 'auto' }}
+            >
+                File Name
+            </Column>
+            <Column<SC.DataFile>
+                Key={'DataStartTime'}
+                AllowSort={true}
+                Field={'DataStartTime'}
+                HeaderStyle={{ width: 'auto' }}
+                RowStyle={{ width: 'auto' }}
+                Content={({ item, field }) => {
+                    if (item[field] == "0001-01-01T00:00:00")
+                        return 'N/A'
+                    return <span className={`badge badge-pill badge-info`}>{moment(item[field]).format('MM/DD/YYYY hh:mm')}</span>
+                }}
+            >
+                Data Start Time
+            </Column>
+            <Column<SC.DataFile>
+                Key={'ProcessingStartTime'}
+                AllowSort={true}
+                Field={'ProcessingStartTime'}
+                HeaderStyle={{ width: 'auto' }}
+                RowStyle={{ width: 'auto' }}
+                Content={({ item, field }) => {
+                    if (item[field] == null || item[field] == undefined)
+                        return 'N/A'
+                    return <span className={`badge badge-pill badge-info`}>{moment(item[field]).format('MM/DD/YYYY hh:mm')}</span>
+                }}
+            >
+                Processing Start Time
+            </Column>
+            <Column<SC.DataFile>
+                Key={'ProcessingEndTime'}
+                AllowSort={true}
+                Field={'ProcessingEndTime'}
+                HeaderStyle={{ width: 'auto' }}
+                RowStyle={{ width: 'auto' }}
+                Content={({ item, field }) => {
+                    if (item[field] == "0001-01-01T00:00:00")
+                        return 'N/A'
+                    return <span className={`badge badge-pill badge-info`}>{moment(item[field]).format('MM/DD/YYYY hh:mm')}</span>
+                }}
+            >
+                Processing End Time
+            </Column>
+            <Column<SC.DataFile>
+                Key={'ProcessingState'}
+                AllowSort={true}
+                Field={'ProcessingState'}
+                HeaderStyle={{ width: 'auto' }}
+                RowStyle={{ width: 'auto' }}
+                Content={({ item, field }) => {
+                    return <ProcessingStatus Status={item[field] as number} FileGroupID={item.FileGroupID} Interactive={false} />
+                }}
+            >
+            </Column>
+        </Table>
+        <Paging Current={page + 1} Total={totalPages} SetPage={(p) => setPage(p - 1)} />
+    </>
+}
+
+export default FilesProcessedTable
