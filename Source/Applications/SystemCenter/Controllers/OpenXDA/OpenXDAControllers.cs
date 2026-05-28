90fca5e732d7153890c23a42bec50fec84e89ac6
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-05-28 15:50:07 -0400

    select one datafile from each group based on filesize

--- Source/Applications/SystemCenter/Controllers/OpenXDA/OpenXDAControllers.cs
+++ Source/Applications/SystemCenter/Controllers/OpenXDA/OpenXDAControllers.cs
@@ -127,7 +127,7 @@ namespace SystemCenter.Controllers.OpenXDA
 
             string sql = $@" 
                 SELECT * ,
-                (SELECT dataFile.FilePath FROM DataFile dataFile WHERE dataFile.FileGroupID = DetailedDataOperationFailure.FileGroupID) as FilePath
+                (SELECT TOP 1 dataFile.FilePath FROM DataFile dataFile WHERE dataFile.FileGroupID = DetailedDataOperationFailure.FileGroupID ORDER BY dataFile.FileSize DESC) as FilePath
                 FROM({CustomView}) DetailedDataOperationFailure
                 WHERE {conditions}
                 ORDER BY {postData.OrderBy} {(postData.Ascending ? "ASC" : "DESC")}
