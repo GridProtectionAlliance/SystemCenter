ab744e4851fc30afa72f771fe0b3df4cfe76fcfb
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-09-01 12:58:06 -0400

    improve table styling

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AppHost/AnalysisTaskTable.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/AppHost/AnalysisTaskTable.tsx
@@ -93,8 +93,6 @@ const AnalysisTaskTable = () => {
                         Key={'MeterName'}
                         AllowSort={false}
                         Field={'MeterName'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto' }}
                     >
                         Meter
                     </Column>
@@ -103,8 +101,6 @@ const AnalysisTaskTable = () => {
                         Key={'DataStartTime'}
                         AllowSort={true}
                         Field={'DataStartTime'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto', textAlign: 'center' }}
                         Content={({ item, field }) => {
                             if (item[field] == "0001-01-01T00:00:00")
                                 return 'N/A'
@@ -118,8 +114,6 @@ const AnalysisTaskTable = () => {
                         Key={'DataEndTime'}
                         AllowSort={true}
                         Field={'DataEndTime'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto', textAlign: 'center' }}
                         Content={({ item, field }) => {
                             if (item[field] == "0001-01-01T00:00:00")
                                 return 'N/A'
@@ -132,8 +126,6 @@ const AnalysisTaskTable = () => {
                         Key={'Priority'}
                         AllowSort={true}
                         Field={'Priority'}
-                        HeaderStyle={{ width: 'auto' }}
-                        RowStyle={{ width: 'auto', textAlign: 'center' }}
                         Content={({ item, field }) =>
                             <Priority
                                 priority={item[field] as number}
