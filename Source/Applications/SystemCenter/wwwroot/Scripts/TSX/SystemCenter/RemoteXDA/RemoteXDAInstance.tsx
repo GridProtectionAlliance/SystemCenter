7abbd74b309aa7a8945c8a7e6f2e3288329f950f
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-09-25 09:44:56 -0400

    use new generic components for remote xda tabbed record layout

--- Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/RemoteXDA/RemoteXDAInstance.tsx
+++ Source/Applications/SystemCenter/wwwroot/Scripts/TSX/SystemCenter/RemoteXDA/RemoteXDAInstance.tsx
@@ -23,85 +23,38 @@
 
 import * as React from 'react';
 import * as _ from 'lodash';
-import { LoadingScreen, TabSelector, Warning } from '@gpa-gemstone/react-interactive';
-import { useAppDispatch, useAppSelector } from '../hooks';
-import { RemoteXDAInstanceSlice } from '../Store/Store';
 import { Application, OpenXDA } from '@gpa-gemstone/application-typings';
 import SystemSettingsTab from './SystemSettingsTab'
 import RemoteAssetTab from './RemoteAssetTab'
 import RemoteMeterTab from './RemoteMeterTab';
+import GenericRecord, { IRecordTab } from '../CommonComponents/GenericRecord';
 
 declare var homePath: string;
 declare type Tab = 'systemSettings' | 'remoteMeter' | 'remoteAsset'
 
 interface IProps { Roles: Array<Application.Types.SecurityRoleName>, ID: number, Tab: Tab }
 
-function RemoteXDAInstance(props: IProps) {
-    const dispatch = useAppDispatch();
+const tabs: IRecordTab<OpenXDA.Types.RemoteXDAInstance>[] = [
+    { Id: 'systemSettings', Label: "System Settings", Content: () => <SystemSettingsTab/> },
+    { Id: "remoteMeter", Label: "Remote Meter", Content: (rec) => <RemoteMeterTab ID={rec.ID} /> },
+    { Id: "remoteAsset", Label: "Remote Asset", Content: (rec) => <RemoteAssetTab ID={rec.ID} /> }
+]
 
-    const instStatus = useAppSelector(RemoteXDAInstanceSlice.Status) as Application.Types.Status;
-    const connection = useAppSelector((state) => RemoteXDAInstanceSlice.Datum(state, props.ID));
-
-    const [tab, setTab] = React.useState(getTab());
-    const [loading, setLoading] = React.useState<boolean>(false);
-    const [showDelete, setShowDelete] = React.useState<boolean>(false);
-
-
-    React.useEffect(() => {
-        if (instStatus === 'uninitiated' || instStatus === 'changed')
-            dispatch(RemoteXDAInstanceSlice.Fetch());
-    }, [dispatch, instStatus]);
-
-    React.useEffect(() => {
-        const saved = getTab();
-        if (saved !== tab)
-            sessionStorage.setItem('RemoteXDAInstance.Tab', JSON.stringify(tab));
-    }, [tab]);
-
-    function getTab(): Tab {
-        if (props.Tab != undefined) return props.Tab;
-        else if (sessionStorage.hasOwnProperty('RemoteXDAInstance.Tab'))
-            return JSON.parse(sessionStorage.getItem('RemoteXDAInstance.Tab'));
-        else
-            return 'systemSettings';
-    }
-
-    function deleteConnection(): void {
-        setLoading(true);
-        if (props.Roles.includes('Administrator'))
-            dispatch(RemoteXDAInstanceSlice.DBAction({ verb: 'DELETE', record: connection }));
-        setLoading(false);
-        window.location.href = homePath + 'index.cshtml?name=RemoteXDAInstanceMain';
-    };
-
-    const Tabs = [
-        { Id: "systemSettings", Label: "System Settings" },
-        { Id: "remoteMeter", Label: "Remote Meters" },
-        { Id: "remoteAsset", Label: "Remote Assets" },
-    ];
-
-    if (connection == null) return null;
-
+function RemoteXDAInstance({Roles, ID, Tab }: IProps) {
     return (
-        <div style={{ width: '100%', height: '100%', overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
-            <div className="row p-2">
-                <div className="col">
-                    <h2>{connection.Name}</h2>
-                </div>
-                <div className="col">
-                    <button className="btn btn-danger pull-right" disabled={!props.Roles.includes('Administrator')} onClick={() => setShowDelete(true)}>Delete Remote Connection</button>
-                </div>
-            </div>
-            <hr />
-            <TabSelector CurrentTab={tab} SetTab={(t: Tab) => setTab(t)} Tabs={Tabs} />
-            {tab === 'systemSettings' ? <SystemSettingsTab ID={props.ID} /> : null}
-            {tab === 'remoteMeter' ? <RemoteMeterTab ID={props.ID} /> : null}
-            {tab === 'remoteAsset' ? <RemoteAssetTab ID={props.ID} /> : null}
-
-            <Warning Message={'This will permanently delete this Remote openXDA Instance Connection and cannot be undone.'} Show={showDelete} Title={'Delete ' + (connection?.Name ?? 'Remote Connection')} CallBack={(conf) => { if (conf) deleteConnection(); setShowDelete(false); }} />
-            <LoadingScreen Show={loading} />
-        </div>
+        <GenericRecord<OpenXDA.Types.RemoteXDAInstance>
+            RecordID={ID}
+            RecordType={"Remote XDA Instance"}
+            TabLocalStorage={"RemoteXDAInstance.Tab"}
+            ControllerPath={`${homePath}api/OpenXDA/remoteXDAInstance`}
+            DefaultSort={"Name"}
+            GetOnePath={`${homePath}api/OpenXDA/remoteXDAInstance/One/`}
+            Redirect={`${homePath}index.cshtml?name=RemoteXDAInstanceMain`}
+            DefaultTab={"systemSettings"}
+            GetName={(record) => record.Name}
+            Tabs={tabs}
+            HasDeletePermission={() => Roles.includes('Administrator')}
+        />
     )
 }
-
 export default RemoteXDAInstance;
